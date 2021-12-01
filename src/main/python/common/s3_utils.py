66c8bad3fa1946c2313e8f758f82b612efa3bb98
matt-koson <matt.koson@hp.com>
2021-12-01 11:08:30 -0800

    repo cleanup

--- src/main/python/common/s3_utils.py
+++
@@ -1,7 +0,0 @@
-# Databricks notebook source
-# mount S3 bucket to cluster
-def s3_mount (bucket_name, mount_name):
-  try:
-    dbutils.fs.mount(f"s3a://{bucket_name}", f"/mnt/{mount_name}")
-  except:
-    print("WARNING: mount does not exist or is already mounted to cluster")
--- src/main/scala/main.scala
+++
@@ -1,9 +0,0 @@
-package main.scala
-
-object main extends App {
-  helloWorld("World")
-
-  def helloWorld(string: String): Unit = {
-    println("Hello, " + string + "!")
-  }
-}
