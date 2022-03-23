f192a451baa4bb9518dfd9a396c6c7ab0fded5c3
Matt Koson <matt.koson@hp.com>
2022-03-23 13:55:34 -0700

    Update MoveSfaiDataToRedshift.scala

--- notebooks/scala/mdm/MoveSfaiDataToRedshift.scala
+++ notebooks/scala/mdm/MoveSfaiDataToRedshift.scala
@@ -35,7 +35,7 @@ tableDF.createOrReplaceTempView("tableDF")
 
 val inputTableCols = tableDF.columns.map(x => x.toLowerCase)
 
-val schema = if(List("decay", "hardware_ltf", "version").contains(table)) "prod" else "mdm"
+val schema = if(List("decay", "hardware_ltf", "version").contains(configs("table"))) "prod" else "mdm"
 
 val outputTableCols = readRedshiftToDF(configs)
   .option("dbtable", s"""${schema}.${configs("table")}""")
@@ -45,14 +45,14 @@ val outputTableCols = readRedshiftToDF(configs)
 var query = "SELECT \n"
 
 for (col <- outputTableCols; if (!(List("cal_id", "decay_id", "geo_id", "hw_ltf_id", "iso_cc_id", "pl_id", "profit_center_id", "rdma_id", "sup_hw_id", "sup_xref_id", "yield_id").contains(col)))) {
-  if (col == "id" && table == "hardware_xref") {} else {
+  if (col == "id" && configs("table") == "hardware_xref") {} else {
     if (inputTableCols.contains(col)) {
       query = query + col
     } else {
       if (col == "official") query = query + "1 AS official"
       if (col == "last_modified_date") query = query + "load_date AS last_modified_date"
-      if (col == "profit_center_code" && table == "product_line_xref") query = query + "profit_center AS profit_center_code"
-      if (col == "load_date") query = query + s"""CAST(\"${redshiftTimestamp}\" AS TIMESTAMP) AS load_date"""
+      if (col == "profit_center_code" && configs("table") == "product_line_xref") query = query + "profit_center AS profit_center_code"
+      if (col == "load_date") query = query + s"""CAST(\"${configs("redshiftTimestamp")}\" AS TIMESTAMP) AS load_date"""
     }
     
     if (outputTableCols.dropRight(1).contains(col)) query = query + ","
@@ -70,7 +70,7 @@ writeDFToS3(finalTableDF, s"s3a://dataos-core-${stack}-team-phoenix/landing/${co
 
 // truncate existing redshift data and
 // write data to redshift
-writeDFToRedshift(configs, finalTableDF, s"${schema}.${configs("table")}", "append", "CSV GZIP", "", s"TRUNCATE ${schema}.${configs("table")}")
+writeDFToRedshift(configs, finalTableDF, s"${configs("stack")}.${configs("table")}", "append", "CSV GZIP", "", s"TRUNCATE ${schema}.${configs("table")}")
 
 // COMMAND ----------
 
