f04a469e7bdeee5f25d15d06964092524b4f555e
Brent Merrick <brent.merrick@hp.com>
2022-03-25 16:01:10 -0600

    update flash and wd3 json files and addversion sproc

--- stored_procedures/addversion_sproc.sql
+++ stored_procedures/addversion_sproc.sql
@@ -2,83 +2,79 @@ CREATE OR REPLACE PROCEDURE prod.addversion_sproc(v1_record varchar, v2_source_n
 	LANGUAGE plpgsql
 AS $$
 	
-	
-	
-	
-declare 
+declare
 record_count integer;
 max_version text;
 
 begin
 
 /* Built 1/1/2022 - Brent Merrick
-* This function adds records to the prod.version table for 
-* different datasets 
-*/	
-	
+* This function adds records to the prod.version table for
+* different datasets
+*/
+
 select COUNT(1) into record_count
 from prod.version
 where 1=1
 	and record = v1_record
-	and version = (convert(char(4), date_part(y,getdate())) 
-			+ '.' + convert(char(2), date_part(month,getdate())) 
-			+ '.' + convert(char(4), date_part(d,getdate()))
+	and version = (convert(char(4), date_part(y,getdate()))
+			+ '.' + to_char(date_part(month,getdate()), 'fm00')
+			+ '.' + to_char(date_part(d,getdate()), 'fm00')
 			+ '.1');
-		
+
 select max(version) into max_version
 from prod.version
 where record = v1_record;
 
 IF record_count > 0 then
-		update prod.version 
+		update prod.version
 		set official = 0
 		where 1=1
 			and record = v1_record
 			and record <> 'ib';
 
-		INSERT INTO prod.version 
+		INSERT INTO prod.version
 		(record, version, source_name, official, load_date)
-		VALUES 
+		VALUES
 		(
 		v1_record
-		,convert(char(4), date_part(y,getdate())) 
-			+ '.' + convert(char(2), date_part(month,getdate())) 
-			+ '.' + convert(char(4), date_part(d,getdate()))
+		,convert(char(4), date_part(y,getdate()))
+			+ '.' + to_char(date_part(month,getdate()), 'fm00')
+			+ '.' + to_char(date_part(d,getdate()), 'fm00')
 			+ '.' + cast(cast(right(max_version,1) as int)+1 as text)
 		,v2_source_name
 		,case v1_record
 				when 'ib' then 0
-				else 1 
+				else 1
 			end
 		,getdate()
 		);
 else
-		update prod.version 
+		update prod.version
 		set official = case v1_record
-							when 'ib' then false 
-							else official 
-						end						
+							when 'ib' then false
+							else official
+						end
 		where record = v1_record;
-	
-		INSERT INTO prod.version 
+
+		INSERT INTO prod.version
 		(record, version, source_name, official, load_date)
-		VALUES 
+		VALUES
 		(
 		v1_record
-		,convert(char(4), date_part(y,getdate())) 
-			+ '.' + convert(char(2), date_part(month,getdate())) 
-			+ '.' + convert(char(4), date_part(d,getdate()))
+		,convert(char(4), date_part(y,getdate()))
+			+ '.' + to_char(date_part(month,getdate()), 'fm00')
+			+ '.' + to_char(date_part(d,getdate()), 'fm00')
 			+ '.1'
 		,v2_source_name
 		,case v1_record
 						when 'ib' then 0
-						else 1 
+						else 1
 					end
 		,getdate()
 		);
 end if;
 end;
-
 
 $$
 ;
