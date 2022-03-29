19fc17921ec1f5300674c115df3d33e08b31ca19
matt.koson@hp.com <matt.koson@hp.com>
2022-03-29 17:50:26 +0000

    Update hardware_stf notebook

--- notebooks/python/hardware_stf/hardware_stf.py
+++ notebooks/python/hardware_stf/hardware_stf.py
@@ -1,17 +1,17 @@
 # Databricks notebook source
 dbutils.widgets.text("redshift_secrets_name", "")
 dbutils.widgets.text("sqlserver_secrets_name", "")
-dbutils.widgets.dropdown("stack", "dev", Seq("dev", "itg", "prod"))
+dbutils.widgets.dropdown("stack", "dev", ["dev", "itg", "prod"])
 dbutils.widgets.text("aws_iam_role", "")
 dbutils.widgets.text("job_dbfs_path", "")
 
 # COMMAND ----------
 
-# MAGIC %run ./common/database_utils
+# MAGIC %run ../common/database_utils
 
 # COMMAND ----------
 
-# MAGIC %run ./common/secrets_manager_utils
+# MAGIC %run ../common/secrets_manager_utils
 
 # COMMAND ----------
 
@@ -99,10 +99,10 @@ product_line_xref.createOrReplaceTempView("product_line_xref")
 
 # COMMAND ----------
 
-ltf_version = spark.sql("""SELECT MAX(version) AS version FROM hardware_ltf WHERE record = 'hw_fcst'""").head()[0]
+ltf_version = spark.sql("""SELECT MAX(version) AS version FROM hardware_ltf WHERE UPPER(record) = 'HW_FCST'""").head()[0]
 print("ltf_version: " + ltf_version)
 
-ltf_record = "hw_fcst"
+ltf_record = "HW_FCST"
 print("ltf_record: " + ltf_record)
 
 wd3_max_cal_date = str(spark.sql("""SELECT MAX(cal_date) AS date FROM wd3 WHERE record LIKE ('WD3%')""").head()[0])
@@ -133,9 +133,9 @@ SELECT DISTINCT
   , c.category_feature
 FROM hardware_ltf a
 LEFT JOIN rdma b
-  ON a.base_product_number=b.Base_Prod_Number
+  ON UPPER(a.base_product_number)=UPPER(b.base_prod_number)
 LEFT JOIN hardware_xref c
-  ON b.Platform_Subset=c.platform_subset
+  ON UPPER(b.platform_subset)=UPPER(c.platform_subset)
 LEFT JOIN iso_country_code_xref d
   ON a.country_alpha2=d.country_alpha2
 WHERE 1=1
@@ -156,9 +156,9 @@ SELECT DISTINCT
 	, c.category_feature
 FROM flash a
 LEFT JOIN rdma b
-  ON a.base_product_number=b.Base_Prod_Number
+  ON UPPER(a.base_product_number)=UPPER(b.base_prod_number)
 LEFT JOIN hardware_xref c
-  ON b.Platform_Subset=c.platform_subset
+  ON UPPER(b.platform_subset)=UPPER(c.platform_subset)
 LEFT JOIN iso_country_code_xref d
   ON a.country_alpha2=d.country_alpha2
 ORDER BY a.cal_date
@@ -176,9 +176,9 @@ SELECT DISTINCT
 	, c.category_feature
 FROM wd3 a
 LEFT JOIN rdma b
-  ON a.base_product_number=b.base_prod_number
+  ON UPPER(a.base_product_number)=UPPER(b.base_prod_number)
 LEFT JOIN hardware_xref c
-  ON b.Platform_Subset=c.platform_subset
+  ON UPPER(b.Platform_Subset)=UPPER(c.platform_subset)
 LEFT JOIN iso_country_code_xref d
   ON a.country_alpha2=d.country_alpha2
 WHERE 1=1
@@ -197,7 +197,7 @@ FROM wd3_allocated_ltf_ltf_combos a
 LEFT JOIN wd3_allocated_ltf_wd3_combos b
 ON a.region_5 = b.region_5
 	AND a.cal_date = b.cal_date
-	AND a.category_feature=b.category_feature
+	AND UPPER(a.category_feature)=UPPER(b.category_feature)
 WHERE b.region_5 IS NULL
 ORDER BY 3,1,2
 """)
@@ -213,7 +213,7 @@ FROM wd3_allocated_ltf_flash_combos a
 LEFT JOIN wd3_allocated_ltf_wd3_combos b
 ON a.region_5 = b.region_5
 	AND a.cal_date = b.cal_date
-	AND a.category_feature=b.category_feature
+	AND UPPER(a.category_feature)=UPPER(b.category_feature)
 WHERE b.region_5 IS NULL
 ORDER BY 3,1,2
 """)
@@ -230,11 +230,11 @@ SELECT
 	, c.category_feature
 	, SUM(a.units) AS units
 FROM hardware_ltf a
-LEFT JOIN rdma b ON a.base_product_number=b.Base_Prod_Number
-LEFT JOIN hardware_xref c ON b.Platform_Subset=c.platform_subset
+LEFT JOIN rdma b ON UPPER(a.base_product_number)=UPPER(b.base_prod_number)
+LEFT JOIN hardware_xref c ON UPPER(b.platform_subset)=UPPER(c.platform_subset)
 LEFT JOIN iso_country_code_xref d on a.country_alpha2=d.country_alpha2
 WHERE 1=1
-	AND a.record = "{ltf_record}"
+	AND UPPER(a.record) = "{ltf_record}"
 	AND a.version = "{ltf_version}"
 	AND a.cal_date <= "{wd3_max_cal_date}"
 GROUP BY 
@@ -256,8 +256,8 @@ SELECT
 	, c.category_feature
 	, SUM(a.units) AS units
 FROM flash a
-LEFT JOIN rdma b ON a.base_product_number=b.Base_Prod_Number
-LEFT JOIN hardware_xref c ON b.Platform_Subset=c.platform_subset
+LEFT JOIN rdma b ON UPPER(a.base_product_number)=UPPER(b.base_prod_number)
+LEFT JOIN hardware_xref c ON UPPER(b.platform_subset)=UPPER(c.platform_subset)
 LEFT JOIN iso_country_code_xref d on a.country_alpha2=d.country_alpha2
 GROUP BY 
 	  d.region_5
@@ -282,12 +282,12 @@ SELECT
 	, a.version
 	, sum(a.units) AS units
 FROM wd3 a
-LEFT JOIN rdma b ON a.base_product_number=b.base_prod_number
-LEFT JOIN hardware_xref c ON b.platform_subset=c.platform_subset
+LEFT JOIN rdma b ON UPPER(a.base_product_number)=UPPER(b.base_prod_number)
+LEFT JOIN hardware_xref c ON UPPER(b.platform_subset)=UPPER(c.platform_subset)
 LEFT JOIN iso_country_code_xref d on a.country_alpha2=d.country_alpha2
 WHERE 1=1
 	AND units > 0
-	AND c.technology IN ('INK','LASER','PWA')
+	AND UPPER(c.technology) IN ('INK','LASER','PWA')
 GROUP BY 
 	  a.record
 	, region_5
@@ -333,7 +333,7 @@ FROM wd3_allocated_ltf_wd3_pct a
 INNER JOIN wd3_allocated_ltf_ltf_units b 
 	ON a.cal_date=b.cal_date 
 	AND a.region_5=b.region_5
-	AND a.category_feature = b.category_feature
+	AND UPPER(a.category_feature) = UPPER(b.category_feature)
 WHERE 1=1;
 """)
 
@@ -354,7 +354,7 @@ FROM wd3_allocated_ltf_wd3_pct a
 INNER JOIN wd3_allocated_ltf_flash_units b 
 	ON a.cal_date=b.cal_date 
 	AND a.region_5=b.region_5
-	AND a.category_feature = b.category_feature
+	AND UPPER(a.category_feature) = UPPER(b.category_feature)
 WHERE 1=1;
 """)
 
@@ -372,15 +372,15 @@ SELECT
 	, c.category_feature
 	, SUM(a.units) as units
 FROM hardware_ltf a
-LEFT JOIN rdma b ON a.base_product_number=b.Base_Prod_Number
-LEFT JOIN hardware_xref c ON b.Platform_Subset=c.platform_subset
+LEFT JOIN rdma b ON UPPER(a.base_product_number)=UPPER(b.Base_Prod_Number)
+LEFT JOIN hardware_xref c ON UPPER(b.Platform_Subset)=UPPER(c.platform_subset)
 LEFT JOIN iso_country_code_xref d on a.country_alpha2=d.country_alpha2
 INNER JOIN wd3_allocated_ltf_missing_ltf_combos f
-	ON c.category_feature=f.category_feature
+	ON UPPER(c.category_feature)=UPPER(f.category_feature)
 	AND a.cal_date = f.cal_date
 	AND d.region_5=f.region_5
 WHERE 1=1
-	AND a.record = "{ltf_record}"
+	AND UPPER(a.record) = "{ltf_record}"
 	AND a.version = "{ltf_version}"
 	AND a.cal_date <= "{wd3_max_cal_date}"
 GROUP BY
@@ -405,11 +405,11 @@ SELECT
 	, c.category_feature
 	, SUM(a.units) as units
 FROM flash a
-LEFT JOIN rdma b ON a.base_product_number=b.base_prod_number
-LEFT JOIN hardware_xref c ON b.platform_subset=c.platform_subset
+LEFT JOIN rdma b ON UPPER(a.base_product_number)=UPPER(b.base_prod_number)
+LEFT JOIN hardware_xref c ON UPPER(b.platform_subset)=UPPER(c.platform_subset)
 LEFT JOIN iso_country_code_xref d on a.country_alpha2=d.country_alpha2
 INNER JOIN wd3_allocated_ltf_missing_flash_combos f
-	ON c.category_feature=f.category_feature
+	ON UPPER(c.category_feature)=UPPER(f.category_feature)
 	AND a.cal_date = f.cal_date
 	AND d.region_5=f.region_5
 GROUP BY
@@ -452,45 +452,16 @@ wd3_allocated_ltf_final.createOrReplaceTempView("wd3_allocated_ltf_final")
 
 # COMMAND ----------
 
-# --add record to version table
-submit_remote_query(configs["redshift_dbname"], configs["redshift_port"], configs["redshift_username"], configs["redshift_password"], configs["redshift_url"], """CALL prod.addversion_sproc('allocated_ltf', 'wd3_allocated_flash_plus_ltf');""")
-
-# COMMAND ----------
-
-version_query = """
-SELECT
-      record
-	, MAX(version) AS version
-	, MAX(load_date) as load_date
-FROM prod.version
-WHERE record = 'allocated_ltf'
-GROUP BY record
-"""
-
-version = read_redshift_to_df(configs) \
-  .option("query", version_query) \
-  .load()
-
-max_allocated_version = version.select("version").distinct().head()[0]
-max_allocated_load_date = version.select("load_date").distinct().head()[0]
-
-print("max_allocated_version: " + max_allocated_version)
-print("max_allocated_load_date: " + str(max_allocated_load_date))
-
-# COMMAND ----------
-
 # --load data to allocated_ltf_landing table
 # --update load_date
 allocated_ltf_landing = spark.sql(f"""
 SELECT
-	  'stf' AS record
+	  'STF' AS record
 	, cal_date
 	, country_alpha2
 	, base_product_number
 	, SUM(units) AS units
-    , CAST("{max_allocated_load_date}" as date) AS load_date
 	, source
-	,"{max_allocated_version}" AS version
 FROM wd3_allocated_ltf_final 
 WHERE 1=1
 GROUP BY
@@ -507,14 +478,11 @@ allocated_ltf_landing.createOrReplaceTempView("allocated_ltf_landing")
 # --load latest stitched dataset to hardware_stf landing table
 hardware_stf_landing = spark.sql("""
 SELECT 
-	 'allocated flash plus ltf' AS record
+	 'ALLOCATED FLASH PLUS LTF' AS record
     , country_alpha2 AS geo
-	, 'CtryCd' as geo_type
     , base_product_number as base_prod_number
     , cal_date as date
 	, units
-    , load_date
-    , version
 FROM allocated_ltf_landing
 """)
 
@@ -523,7 +491,7 @@ hardware_stf_landing.createOrReplaceTempView("hardware_stf_landing")
 # COMMAND ----------
 
 # --Add version to version table
-submit_remote_query(configs["redshift_dbname"], configs["redshift_port"], configs["redshift_username"], configs["redshift_password"], configs["redshift_url"], """CALL prod.addversion_sproc('hw_stf_fcst', 'Archer');""")
+submit_remote_query(configs["redshift_dbname"], configs["redshift_port"], configs["redshift_username"], configs["redshift_password"], configs["redshift_url"], """CALL prod.addversion_sproc('HW_STF_FCST', 'ARCHER');""")
 
 # COMMAND ----------
 
@@ -533,7 +501,7 @@ SELECT
 	, MAX(version) AS version
 	, MAX(load_date) as load_date
 FROM prod.version
-WHERE record = 'hw_stf_fcst'
+WHERE record = 'HW_STF_FCST'
 GROUP BY record
 """
 
@@ -553,7 +521,7 @@ print("max_forecast_load_date: " + str(max_forecast_load_date))
 # --UPDATE staging load_date and version
 hardware_stf_staging = spark.sql(f"""
 SELECT DISTINCT
-     'hw_stf_fcst' AS record
+     'HW_STF_FCST' AS record
 	, s.record AS forecast_name
 	, s.date AS cal_date
 	, i.region_5
@@ -565,7 +533,7 @@ SELECT DISTINCT
 	, CAST('true' AS BOOLEAN) AS official
 	, "{max_forecast_version}" AS version
 FROM hardware_stf_landing s
-	LEFT JOIN rdma ON rdma.base_prod_number=s.base_prod_number
+	LEFT JOIN rdma ON UPPER(rdma.base_prod_number)=UPPER(s.base_prod_number)
 	LEFT JOIN iso_country_code_xref i ON s.geo=i.country_alpha2
 """)
 
@@ -573,7 +541,7 @@ hardware_stf_staging.createOrReplaceTempView("hardware_stf_staging")
 
 # COMMAND ----------
 
-submit_remote_query(configs["redshift_dbname"], configs["redshift_port"], configs["redshift_username"], configs["redshift_password"], configs["redshift_url"], "UPDATE prod.hardware_ltf SET official = 0 WHERE record = 'hw_stf_fcst' AND official=1;")
+submit_remote_query(configs["redshift_dbname"], configs["redshift_port"], configs["redshift_username"], configs["redshift_password"], configs["redshift_url"], "UPDATE prod.hardware_ltf SET official = 0 WHERE UPPER(record) = 'HW_STF_FCST' AND official=1;")
 
 # COMMAND ----------
 
@@ -591,12 +559,12 @@ SELECT DISTINCT
       , a.load_date
       , a.version
 FROM hardware_stf_staging a 
-      LEFT JOIN rdma b ON a.platform_subset=b.Platform_Subset
-	  LEFT JOIN product_line_xref c ON b.pl = c.PL
-WHERE c.Technology IN ('INK','LASER','PWA') AND c.PL_category = 'HW'
-	AND a.platform_subset NOT LIKE ('ACCESSORY %')
-	AND a.platform_subset <> 'MOBILE DONGLE'
-	AND a.platform_subset <> 'PAGEWIDE ACCESSORIES'
+      LEFT JOIN rdma b ON UPPER(a.platform_subset)=UPPER(b.platform_subset)
+	  LEFT JOIN product_line_xref c ON UPPER(b.pl) = UPPER(c.PL)
+WHERE UPPER(c.Technology) IN ('INK','LASER','PWA') AND UPPER(c.pl_category) = 'HW'
+	AND UPPER(a.platform_subset) NOT LIKE ('ACCESSORY %')
+	AND UPPER(a.platform_subset) <> 'MOBILE DONGLE'
+	AND UPPER(a.platform_subset) <> 'PAGEWIDE ACCESSORIES'
 	AND units <> 0
 """)
 
