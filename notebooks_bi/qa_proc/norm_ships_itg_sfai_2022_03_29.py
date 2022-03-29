d078a8897f0498f74b791cd2af9e769619136ebe
mark.middendorf@hp.com <mark.middendorf@hp.com>
2022-03-29 23:42:30 +0000

    another round 03-29-2022
    more qc nb dev

--- notebooks_bi/qa_proc/norm_ships_itg_sfai_2022_03_29.py
+++ notebooks_bi/qa_proc/norm_ships_itg_sfai_2022_03_29.py
@@ -10,12 +10,16 @@
 # MAGIC  + review high-level v2v comparisons
 # MAGIC  + review high-level technology comparisons (Laser, Ink/PWA)
 # MAGIC  + deep dive into corner cases if needed
+# MAGIC  
+# MAGIC **Intelligence:**
+# MAGIC   + https://github.azc.ext.hp.com/supplies-bd/team-phoenix-databricks-jobs-config/blob/master/jobs/actuals-hw/itg.yml
+# MAGIC   + https://spark.apache.org/docs/latest/api/python//reference/index.html
 
 # COMMAND ----------
 
 # MAGIC %md
 # MAGIC 
-# MAGIC ## Notebook setup
+# MAGIC ## Notebook setup - permissions and database utils
 
 # COMMAND ----------
 
@@ -88,8 +92,8 @@ configs["redshift_password"] = redshift_secrets["password"]
 configs["redshift_url"] = constants["REDSHIFT_URLS"][dbutils.widgets.get("stack")]
 configs["redshift_port"] = constants["REDSHIFT_PORTS"][dbutils.widgets.get("stack")]
 configs["redshift_dbname"] = constants["REDSHIFT_DATABASE"][dbutils.widgets.get("stack")]
-# configs["aws_iam_role"] = dbutils.widgets.get("aws_iam_role")
-# configs["redshift_temp_bucket"] =  "{}redshift_temp/".format(constants['S3_BASE_BUCKET'][dbutils.widgets.get("stack")])
+configs["aws_iam_role"] = dbutils.widgets.get("aws_iam_role")
+configs["redshift_temp_bucket"] =  "{}redshift_temp/".format(constants['S3_BASE_BUCKET'][dbutils.widgets.get("stack")])
 configs["sfai_username"] = sqlserver_secrets["username"]
 configs["sfai_password"] = sqlserver_secrets["password"]
 configs["sfai_url"] = constants["SFAI_URL"]
@@ -122,19 +126,45 @@ ns_sfai_records.show()
 
 # COMMAND ----------
 
-# MAGIC %md
-# MAGIC testing pyspark df
+ns_sfai_records.count()
 
 # COMMAND ----------
 
 # MAGIC %md
-# MAGIC ## aggregating pyspark df
+# MAGIC ## testing output - simulate dbt data tests using pyspark.sql.DataFrame
+# MAGIC 
+# MAGIC Below are simple data tests currently used with DBT to check normalized shipments output
 
 # COMMAND ----------
 
-drop_list = ['region_5', 'record', 'country_alpha2', 'platform_subset', 'version']
+# test 1 :: isna() returns a dataframe of boolean - same columns, same number of records
+ns_sfai_records.where('units is null').collect()
+
+# COMMAND ----------
+
+# test 2 :: multiple CKs
+ck = ['region_5', 'cal_date', 'country_alpha2', 'platform_subset']
+ns_sfai_records.groupby(ck).count().filter("count > 1").show()
+
+# COMMAND ----------
+
+# test 3 :: find any generics
+ns_sfai_records.filter(ns_sfai_records.platform_subset.like('%GENERIC%')).collect()
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Shaping using pyspark.pandas.DataFrame for use with matplotlib
+
+# COMMAND ----------
+
 ns_agg_prep = ns_sfai_records.toPandas()
+
+# drop unwanted columns
+drop_list = ['region_5', 'record', 'country_alpha2', 'platform_subset', 'version']
 ns_agg_1 = ns_agg_prep.drop(drop_list, axis=1)
+
+# aggregate time series
 ns_agg_2 = ns_agg_1.groupby(['cal_date'], as_index=False).sum().sort_values('cal_date')
 ns_agg_2['variable'] = 'dbt.norm_ships'
 
@@ -144,6 +174,7 @@ display(ns_agg_2)
 
 # COMMAND ----------
 
+# re-order dataframe columns
 ns_agg_3 = ns_agg_2.reindex(['cal_date', 'variable', 'units'], axis=1)
 
 # COMMAND ----------
@@ -193,3 +224,31 @@ mpl_ts(df=ns_df, format_dict=plot_dict_a)
 # MAGIC TODO:
 # MAGIC   + dbt tests applied to spark df
 # MAGIC   + spark.df to pandas.df --> concat with sfai --> plot
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## REDSHIFT review
+
+# COMMAND ----------
+
+ns_rs_records = read_redshift_to_df(configs) \
+  .option("query", "select * from itg.stage.norm_ships") \
+  .load()
+
+# COMMAND ----------
+
+ns_rs_records.show()
+
+# COMMAND ----------
+
+ns_rs_records.select('record').distinct().collect()
+
+# COMMAND ----------
+
+ns_rs_filter = ns_rs_records.filter("record not in ('ACTUALS_LF', 'HW_LTF_LF')")
+ns_rs_filter.show()
+
+# COMMAND ----------
+
+ns_rs_filter.count()
