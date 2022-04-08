c1c970b444cad5e141b89da1a1f3302f7c554b85
brent.merrick@hp.com <brent.merrick@hp.com>
2022-04-08 19:08:03 +0000

    archive notebook update

--- notebooks/python/archive_ib/archive_ib.py
+++ notebooks/python/archive_ib/archive_ib.py
@@ -1,13 +1,5 @@
 # Databricks notebook source
-dbutils.widgets.text("redshift_secrets_name", "")
-dbutils.widgets.text("sqlserver_secrets_name", "")
-dbutils.widgets.text("aws_iam_role", "")
-dbutils.widgets.text("stack", "")
-dbutils.widgets.text("job_dbfs_path", "")
-
-# COMMAND ----------
-
-# MAGIC %run ../common/secrets_manager_utils
+# MAGIC %run ../common/configs
 
 # COMMAND ----------
 
@@ -16,38 +8,6 @@ dbutils.widgets.text("job_dbfs_path", "")
 # COMMAND ----------
 
 # MAGIC %run ../common/database_utils
-
-# COMMAND ----------
-
-import json
-
-with open(dbutils.widgets.get("job_dbfs_path").replace("dbfs:", "/dbfs") + "/configs/constants.json") as json_file:
-  constants = json.load(json_file)
-
-# COMMAND ----------
-
-# retrieve secrets based on incoming/inputted secrets name - variables will be accessible across languages
-
-redshift_secrets = secrets_get(dbutils.widgets.get("redshift_secrets_name"), "us-west-2")
-
-sqlserver_secrets = secrets_get(dbutils.widgets.get("sqlserver_secrets_name"), "us-west-2")
-
-# COMMAND ----------
-
-configs = {}
-
-configs["redshift_username"] = redshift_secrets["username"]
-configs["redshift_password"] = redshift_secrets["password"]
-configs["redshift_url"] = constants["REDSHIFT_URLS"][dbutils.widgets.get("stack")]
-configs["redshift_port"] = constants["REDSHIFT_PORTS"][dbutils.widgets.get("stack")]
-configs["redshift_dbname"] = constants["REDSHIFT_DATABASE"][dbutils.widgets.get("stack")]
-configs["aws_iam_role"] = dbutils.widgets.get("aws_iam_role")
-configs["redshift_temp_bucket"] =  "{}redshift_temp/".format(constants['S3_BASE_BUCKET'][dbutils.widgets.get("stack")])
-configs["redshift_dev_group"] = constants["REDSHIFT_DEV_GROUP"][dbutils.widgets.get("stack")]
-
-configs["sfai_username"] = sqlserver_secrets["username"]
-configs["sfai_password"] = sqlserver_secrets["password"]
-configs["sfai_url"] = constants["SFAI_URL"]
 
 # COMMAND ----------
 
@@ -65,7 +25,7 @@ SELECT
     ,load_date
     ,version
   FROM archive.dbo.ib_archive
-  WHERE version = '2020.11.03.1'
+  WHERE version = '2021.12.14.1'
   
 """
 
@@ -87,9 +47,13 @@ version = redshift_ib_archive_records.select('version').distinct().head()[0]
 
 # write to parquet file in s3
 
-# s3a://dataos-core-dev-team-phoenix/product/flash/Flash-2022-03/
-s3_flash_output_bucket = constants["S3_BASE_BUCKET"][dbutils.widgets.get("stack")] + "spectrum/ib/" + version
+# s3a://dataos-core-prod-team-phoenix/spectrum/ib/
+s3_ib_output_bucket = constants["S3_BASE_BUCKET"][stack] + "spectrum/ib/" + version
 
-write_df_to_s3(redshift_ib_archive_records, s3_flash_output_bucket, "parquet", "overwrite")
+write_df_to_s3(redshift_ib_archive_records, s3_ib_output_bucket, "parquet", "overwrite")
+
+
+
+# COMMAND ----------
 
 
