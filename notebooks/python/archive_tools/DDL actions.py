4299d2fdb57c4a88c43c85a973c99a17647fe55a
brent.merrick@hp.cm <brent.merrick@hp.cm>
2022-05-05 21:10:06 +0000

    Merge conflict commit

--- notebooks/python/archive_tools/DDL actions.py
+++ notebooks/python/archive_tools/DDL actions.py
@@ -13,12 +13,11 @@
 
 query = """
 
-create external table phoenix_spectrum.usage_share 
-(
-record varchar(255)
-)
-stored as parquet 
-location 's3://dataos-core-prod-team-phoenix/spectrum/usage_share/2022.01.13.1/';
+create external schema phoenix_spectrum_test
+from data catalog 
+database 'team-phoenix-test'
+iam_role 'arn:aws:iam::740156627385:role/team-phoenix-role'
+create external database if not exists;
 
 """
 
@@ -39,7 +38,7 @@ def submit_spectrum_query(dbname, port, user, password, host, sql_query):
     con.commit()
     cur.close()
 
-submit_spectrum_query("prod", configs["redshift_port"], configs["redshift_username"], configs["redshift_password"], configs["redshift_url"], query)
+submit_spectrum_query("dev", configs["redshift_port"], configs["redshift_username"], configs["redshift_password"], configs["redshift_url"], query)
 
 
 # COMMAND ----------
