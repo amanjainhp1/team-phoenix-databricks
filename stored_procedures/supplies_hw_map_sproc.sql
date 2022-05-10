30c3852c88356284f1e46e664c9613ee876014f7
Daman Singh <daman.singh@hp.com>
2022-05-10 15:40:21 -0700

    Update supplies_hw_map_sproc.sql

--- stored_procedures/supplies_hw_map_sproc.sql
+++ stored_procedures/supplies_hw_map_sproc.sql
@@ -4,10 +4,10 @@ AS $$
 
 BEGIN
 
------------------------------------SET offical FLAG = 1, VERSION = NULL, LoadDate= GETDATE() FOR TEMP_LANDING-------------------------
+-----------------------------------SET offical FLAG = 1, LoadDate= GETDATE() FOR TEMP_LANDING-------------------------
 
 UPDATE stage.supplies_hw_mapping_temp_landing
-SET official = 1, version = NULL, load_date = GETDATE()
+SET official = 1, load_date = GETDATE()
 WHERE is_delete != 1;
 
 ------------------DELETE RECORDS FROM SWM MDM TABLES THAT ARE IN SWM TEMP TABLE -------------------------------
@@ -18,19 +18,6 @@ WHERE supplies_hw_mapping.base_product_number = stm.base_product_number
     AND supplies_hw_mapping.geography = stm.geography
     AND supplies_hw_mapping.platform_subset = stm.platform_subset 
     AND supplies_hw_mapping.customer_engagement = stm.customer_engagement
-
----------------------------------RUN SPROC TO UPDATE VERSION AND LOAD DATE IN PROD TABLE-------------------------------------------
-
-CALL prod.addversion_sproc('SUPPLIES_HW_MAP', 'FORECASTER INPUT');
-
--------------------------------UPDATE TEMP_LANDING WITH PROD VERSION AND LOAD DATE----------------------------
-
-UPDATE  stage.supplies_hw_mapping_temp_landing
-SET 
-    load_date = (SELECT MAX(load_date) FROM prod.version WHERE record = 'SUPPLIES_HW_MAP'),
-    version = (SELECT MAX(version) FROM prod.version WHERE record = 'SUPPLIES_HW_MAP')
-WHERE version is NULL
-    AND is_delete != 1; 
 
 -----------------------------------INSERT RECORDS FROM TEMP TABLE-------------------------------------------------------------------
 
@@ -48,7 +35,6 @@ INSERT INTO mdm.supplies_hw_mapping
     , official
     , last_modified_date
     , load_date
-    , version
 )
 SELECT 'SUPPLIES_HW_MAP' AS record
     , geography_grain
@@ -62,7 +48,6 @@ SELECT 'SUPPLIES_HW_MAP' AS record
     , 1 AS official
     , sm.load_date AS last_modified_date
     , sm.load_date
-    , sm.version
 FROM stage.supplies_hw_mapping_temp_landing sm
 WHERE is_delete != 1; 
 
