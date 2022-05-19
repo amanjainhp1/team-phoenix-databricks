d80e51d4cf4d9403048cef7a45fc75c3d1a0d6fe
brent.merrick@hp.com <brent.merrick@hp.com>
2022-05-19 16:19:04 +0000

    usage share promo updates

--- notebooks/python/archive_tools/migrate_table_SFAI_to_Phoenix.py
+++ notebooks/python/archive_tools/migrate_table_SFAI_to_Phoenix.py
@@ -24,21 +24,22 @@
 
 usage_share_country_spin_landing_query = """
 
-SELECT [record]
-      ,[cal_date]
-      ,[geography_grain]
-      ,[geography]
-      ,[platform_subset]
-      ,[customer_engagement]
-      ,[forecast_process_note]
-      ,[forecast_created_date]
-      ,[data_source]
-      ,[version]
-      ,[measure]
-      ,[units]
-      ,[proxy_used]
-      ,[ib_version]
-      ,[load_date]
+SELECT 
+    UPPER(record) AS record
+    ,cal_date
+    ,UPPER(geography_grain) AS geography_grain
+    ,UPPER(geography) AS geography
+    ,UPPER(platform_subset) AS platform_subset
+    ,UPPER(customer_engagement) AS customer_engagement
+    ,UPPER(forecast_process_note) AS forecast_process_note
+    ,forecast_created_date
+    ,UPPER(data_source) AS data_source
+    ,UPPER(version) AS version
+    ,UPPER(measure) AS measure
+    ,units
+    ,UPPER(proxy_used) AS proxy_used
+    ,UPPER(ib_version) AS ib_version
+    ,load_date
   FROM [IE2_Landing].[dbo].[usage_share_country_spin_landing]
 """
 
@@ -48,25 +49,30 @@ usage_share_country_spin_landing_records = read_sql_server_to_df(configs) \
 
 # COMMAND ----------
 
+write_df_to_redshift(configs, usage_share_country_spin_landing_records, "prod.usage_share_country_spin", "overwrite")
+
+# COMMAND ----------
+
 usage_share_override_mature_landing_query = """
 
-SELECT [record]
-      ,[min_sys_dt]
-      ,[month_num]
-      ,[geography_grain]
-      ,[geography]
-      ,[platform_subset]
-      ,[customer_engagement]
-      ,[forecast_process_note]
-      ,[post_processing_note]
-      ,[forecast_created_date]
-      ,[data_source]
-      ,[version]
-      ,[measure]
-      ,[units]
-      ,[proxy_used]
-      ,[ib_version]
-      ,[load_date]
+SELECT 
+    UPPER(record) AS record
+    ,min_sys_dt
+    ,month_num
+    ,UPPER(geography_grain) AS geography_grain
+    ,UPPER(geography) AS geography
+    ,UPPER(platform_subset) AS platform_subset
+    ,UPPER(customer_engagement) AS customer_engagement
+    ,UPPER(forecast_process_note) AS forecast_process_note
+    ,UPPER(post_processing_note) AS post_processing_note
+    ,forecast_created_date
+    ,UPPER(data_source) AS data_source
+    ,UPPER(version) AS version
+    ,UPPER(measure) AS measure
+    ,units
+    ,UPPER(proxy_used) AS proxy_used
+    ,UPPER(ib_version) AS ib_version
+    ,load_date
   FROM [IE2_Landing].[dbo].[usage_share_override_mature_landing]
 """
 
@@ -76,25 +82,30 @@ usage_share_override_mature_landing_records = read_sql_server_to_df(configs) \
 
 # COMMAND ----------
 
+write_df_to_redshift(configs, usage_share_override_mature_landing_records, "prod.usage_share_override_mature", "overwrite")
+
+# COMMAND ----------
+
 usage_share_override_landing_query = """
 
-SELECT [record]
-      ,[min_sys_dt]
-      ,[month_num]
-      ,[geography_grain]
-      ,[geography]
-      ,[platform_subset]
-      ,[customer_engagement]
-      ,[forecast_process_note]
-      ,[post_processing_note]
-      ,[forecast_created_date]
-      ,[data_source]
-      ,[version]
-      ,[measure]
-      ,[units]
-      ,[proxy_used]
-      ,[ib_version]
-      ,[load_date]
+SELECT 
+    UPPER(record) AS record
+    ,min_sys_dt
+    ,month_num
+    ,UPPER(geography_grain) AS geography_grain
+    ,UPPER(geography) AS geography
+    ,UPPER(platform_subset) AS platform_subset
+    ,UPPER(customer_engagement) AS customer_engagement
+    ,UPPER(forecast_process_note) AS forecast_process_note
+    ,UPPER(post_processing_note) AS post_processing_note
+    ,forecast_created_date
+    ,UPPER(data_source) AS data_source
+    ,UPPER(version) AS version
+    ,UPPER(measure) AS measure
+    ,units
+    ,UPPER(proxy_used) AS proxy_used
+    ,UPPER(ib_version) AS ib_version
+    ,load_date
   FROM [IE2_Landing].[dbo].[usage_share_override_landing]
 """
 
@@ -104,20 +115,25 @@ usage_share_override_landing_records = read_sql_server_to_df(configs) \
 
 # COMMAND ----------
 
+write_df_to_redshift(configs, usage_share_override_landing_records, "prod.usage_share_override_npi", "overwrite")
+
+# COMMAND ----------
+
 usage_share_adjust_query = """
 
-SELECT [record]
-      ,[cal_date]
-      ,[geography_grain]
-      ,[geography]
-      ,[measure]
-      ,[platform_subset]
-      ,[customer_engagement]
-      ,[assumption]
-      ,[official]
-      ,[Units]
-      ,[version]
-      ,[load_date]
+SELECT 
+    UPPER(record) AS record
+    ,cal_date
+    ,UPPER(geography_grain) AS geography_grain
+    ,UPPER(geography) AS geography
+    ,UPPER(measure) AS measure
+    ,UPPER(platform_subset) AS platform_subset
+    ,UPPER(customer_engagement) AS customer_engagement
+    ,UPPER(assumption) AS assumption
+    ,official
+    ,units
+    ,UPPER(version) AS version
+    ,load_date
   FROM [IE2_Landing].[dbo].[usage_share_adjust]
 """
 
@@ -127,18 +143,23 @@ usage_share_adjust_records = read_sql_server_to_df(configs) \
 
 # COMMAND ----------
 
+write_df_to_redshift(configs, usage_share_adjust_records, "prod.usage_share_adjust", "overwrite")
+
+# COMMAND ----------
+
 scenario_usage_share_query = """
 
-SELECT [user_name]
-      ,[load_date]
-      ,[upload_type]
-      ,[scenario_name]
-      ,[geography_grain]
-      ,[geography]
-      ,[platform_subset]
-      ,[customer_engagement]
-      ,[base_product_number]
-      ,[measure]
+SELECT 
+       UPPER(user_name) AS user_name
+      ,load_date
+      ,UPPER(upload_type) AS upload_type
+      ,UPPER(scenario_name) AS scenario_name
+      ,UPPER(geography_grain) AS geography_grain
+      ,UPPER(geography) AS geography
+      ,UPPER(platform_subset) AS platform_subset
+      ,UPPER(customer_engagement) AS customer_engagement
+      ,UPPER(base_product_number) AS base_product_number
+      ,UPPER(measure) AS measure
       ,[min_sys_date]
       ,[month_num]
       ,[value]
@@ -151,17 +172,22 @@ scenario_usage_share_records = read_sql_server_to_df(configs) \
 
 # COMMAND ----------
 
+write_df_to_redshift(configs, scenario_usage_share_records, "prod.scenario_usage_share", "overwrite")
+
+# COMMAND ----------
+
 cartridge_mix_override_query = """
 
-SELECT [record]
-      ,[platform_subset]
-      ,[Crg_Base_Prod_Number]
-      ,[geography]
-      ,[geography_grain]
+SELECT 
+       UPPER([record]) AS record
+      ,UPPER([platform_subset]) AS platform_subset
+      ,UPPER([Crg_Base_Prod_Number]) AS Crg_Base_Prod_Number
+      ,UPPER([geography]) AS geography
+      ,UPPER([geography_grain]) AS geography_grain
       ,[cal_date]
       ,[mix_pct]
-      ,[product_lifecycle_status]
-      ,[customer_engagement]
+      ,UPPER([product_lifecycle_status]) AS product_lifecycle_status
+      ,UPPER([customer_engagement]) AS customer_engagement
       ,[load_date]
       ,[official]
   FROM ie2_prod.[dbo].[cartridge_mix_override]
@@ -173,11 +199,4 @@ cartridge_mix_override_query_records = read_sql_server_to_df(configs) \
 
 # COMMAND ----------
 
-
-write_df_to_redshift(configs, usage_share_country_spin_landing_records, "prod.usage_share_country_spin", "overwrite")
-write_df_to_redshift(configs, usage_share_override_mature_landing_records, "prod.usage_share_override_mature", "overwrite")
-write_df_to_redshift(configs, usage_share_override_landing_records, "prod.usage_share_override_npi", "overwrite")
-write_df_to_redshift(configs, usage_share_adjust_records, "prod.usage_share_adjust", "overwrite")
-write_df_to_redshift(configs, scenario_usage_share_records, "prod.scenario_usage_share", "overwrite")
 write_df_to_redshift(configs, cartridge_mix_override_query_records, "prod.cartridge_mix_override", "overwrite")
-
