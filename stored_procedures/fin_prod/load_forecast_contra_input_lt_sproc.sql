4abeb72b4dc8ffb861f9e3cc95157a6f9ec86dfd
Koson <matt.koson@hp.com>
2022-07-12 14:31:33 -0700

    Add gru_override and contra_input sprocs

---
+++ stored_procedures/fin_prod/load_forecast_contra_input_lt_sproc.sql
@@ -0,0 +1,58 @@
+CREATE OR REPLACE PROCEDURE fin_prod.load_forecast_contra_input_lt_sproc()
+    LANGUAGE plpgsql
+AS $$
+
+BEGIN
+
+--Add version to version table
+CALL prod.addversion_sproc('FORECAST_CONTRA_INPUT_LT', 'FINANCE');
+
+DELETE FROM fin_stage.forecast_contra_input_lt_staging
+WHERE EXISTS (
+    SELECT *
+    FROM fin_prod.forecast_contra_input_ops ST
+    WHERE forecast_contra_input_lt_staging.pl = ST.pl
+    AND forecast_contra_input_lt_staging.region_5 = ST.region_5
+    AND forecast_contra_input_lt_staging.fiscal_yr_qtr = ST.fiscal_yr_qtr
+);
+
+UPDATE fin_prod.forecast_contra_input
+SET official = 0
+WHERE EXISTS (
+    SELECT *
+    FROM fin_stage.forecast_contra_input_lt_staging contra_LT
+    WHERE forecast_contra_input.pl = contra_LT.pl
+    AND forecast_contra_input.region_5 = contra_LT.region_5
+    AND forecast_contra_input.country = contra_LT.country_code
+    AND forecast_contra_input.fiscal_yr_qtr = contra_LT.fiscal_yr_qtr
+);
+
+INSERT INTO fin_prod.forecast_contra_input
+           (record
+           ,pl
+           ,region_5
+           ,country
+		   ,fiscal_yr_qtr
+           ,contra_per_qtr
+           ,official
+           ,version
+           ,load_date
+		   ,base_product_number)
+SELECT 'FORECAST_CONTRA_INPUT' as record
+	  ,pl
+      ,region_5
+      ,country_code as country
+	  ,fiscal_yr_qtr
+      ,contra_per_qtr
+      ,1 as official
+	  ,(SELECT version FROM prod.version WHERE record = 'FORECAST_CONTRA_INPUT_LT' 
+				AND version = (SELECT MAX(version) FROM prod.version WHERE record = 'FORECAST_CONTRA_INPUT_LT')) AS version
+      ,(SELECT load_date FROM prod.version WHERE record = 'FORECAST_CONTRA_INPUT_LT' 
+				AND load_date = (SELECT MAX(load_date) FROM prod.version WHERE record = 'FORECAST_CONTRA_INPUT_LT')) AS load_date
+	  ,NULL as base_product_number
+FROM fin_stage.forecast_contra_input_lt_staging;
+
+END;
+
+$$
+;
---
+++ stored_procedures/fin_stage/load_forecast_contra_input_ops_sproc.sql
@@ -0,0 +1,51 @@
+CREATE OR REPLACE PROCEDURE fin_prod.load_forecast_contra_sproc()
+    LANGUAGE plpgsql
+AS $$
+
+BEGIN
+
+CREATE TABLE IF NOT EXISTS fin_stage.forecast_contra_input_ops_staging(
+    pl VARCHAR(255)
+    ,region_5 VARCHAR(255)
+    ,country_code VARCHAR(255)
+    ,fiscal_yr_qtr VARCHAR(255)
+    ,contra_per_qtr DOUBLE PRECISION
+    ,base_product_number VARCHAR(50)
+);
+GRANT ALL ON TABLE fin_stage.forecast_contra_input_ops_staging TO auto_glue;
+
+---------------------TRUNCATE FORECAST CONTRA TABLE IN fin_prod----------------------------------
+
+TRUNCATE TABLE fin_stage.forecast_contra_input_ops_staging;
+
+-------------------------------------POPULATE FORECAST CONTRA TABLE IN fin_prod FROM fin_stage------------------------------------
+INSERT INTO fin_stage.forecast_contra_input_ops_staging
+(
+ pl
+,region_5
+,country
+,fiscal_yr_qtr
+,contra_per_qtr
+,base_product_number
+)
+SELECT 
+ pl
+,region_5
+,country
+,fiscal_yr_qtr
+,contra_per_qtr
+,base_product_number
+FROM fin_stage.forecast_contra_input_ops_landing;
+
+---------------------------------------------------------MOVE DATA TO OFFICIAL FINANCIAL TABLE-------------------------------------
+
+CALL fin_prod.load_forecast_contra_input_ops_sproc;
+
+--------------------------------------------DROP TABLE------------------------------------------------------
+
+DROP TABLE fin_stage.forecast_contra_input_ops_landing;
+
+END;
+
+$$
+;
