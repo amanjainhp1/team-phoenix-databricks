dd4e347763ac6ffff70d8f07438aa9729e17175f
brent.merrick@hp.com <brent.merrick@hp.com>
2022-07-13 22:04:50 +0000

    tools updates

--- notebooks/python/archive_tools/copy_phoenix_to_SFAI.py
+++ notebooks/python/archive_tools/copy_phoenix_to_SFAI.py
@@ -13,38 +13,32 @@
 
 query = """
 
-SELECT [record]
-      ,[cal_date]
-      ,[geography_grain]
-      ,[geography]
-      ,[measure]
-      ,[platform_subset]
-      ,[customer_engagement]
-      ,[assumption]
-      ,[official]
-      ,[Units]
-      ,[version]
-      ,[load_date]
-  FROM ie2_landing.[dbo].[usage_share_adjust]
+select
+	record,
+	forecast_name,
+	cal_date,
+	country_alpha2,
+	platform_subset,
+	base_product_number,
+	units,
+	load_date,
+	official,
+	version
+from
+	prod.hardware_ltf
+where
+	1 = 1
+	and record = 'HW_FCST'
+	and version = '2022.06.02.1'
+order by
+	version
 """
 
-records = read_sql_server_to_df(configs) \
+
+redshift_ltf_records = read_redshift_to_df(configs) \
     .option("query", query) \
     .load()
 
 # COMMAND ----------
 
-write_df_to_redshift(configs, records, "prod.usage_share_adjust", "overwrite")
-
-# COMMAND ----------
-
-# output dataset to S3 for archival purposes
-# write to parquet file in s3
-
-# s3a://dataos-core-dev-team-phoenix/product/list_price_gpsy/
-
-#when we move this to PROD, we'll need to be sure and write out .parquet files for backup reason.  I didn't do this on 5/5/22 when inserting data into the DEV environment.
-
-s3_output_bucket = constants["S3_BASE_BUCKET"][stack] + "product/list_price_gpsy/" + max_version
-
-write_df_to_s3(records, s3_output_bucket, "parquet", "overwrite")
+write_df_to_sqlserver(configs, redshift_ltf_records, "ie2_prod.dbo.hardware_ltf", "append")
