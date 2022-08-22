e773a6aef0e11cc09368fc545d94614a46d6bbfb
Matt Koson <matt.koson@hp.com>
2022-08-22 09:48:44 -0700

    Add splunk to dev env

--- notebooks/python/example/example_notebook.py
+++ notebooks/python/example/example_notebook.py
@@ -7,4 +7,14 @@ print("this task is: " + task)
 
 # COMMAND ----------
 
+import dataos_splunk 
+import os
+
+data = {"hello": "from niraj"} 
+metadata = {"source": "disk"} 
+
+dataos_splunk.send_to_splunk(metadata, data) 
+
+# COMMAND ----------
+
 dbutils.notebook.exit("SUCCESS: " + task)
