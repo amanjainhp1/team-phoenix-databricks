eaeca04e5537b9610efaa40c6047554c28fe36cd
brent.merrick@hp.com <brent.merrick@hp.com>
2022-08-24 21:00:52 +0000

    update tools

--- notebooks/python/archive_tools/compare itg IB to prod IB from parquet.py
+++ notebooks/python/archive_tools/compare itg IB to prod IB from parquet.py
@@ -12,15 +12,15 @@
 # COMMAND ----------
 
 prod_ib = spark.read.parquet('s3://insights-environment-sandbox/BrentT/ib')
-prod_ib.count()
+# prod_ib.count()
 prod_ib.createOrReplaceTempView('prod_ib')
 
-%sql
-SELECT COUNT(*) FROM prob_ib
+
 
 # COMMAND ----------
 
-
+# MAGIC %sql
+# MAGIC SELECT * FROM prod_ib
 
 # COMMAND ----------
 
@@ -40,6 +40,45 @@ redshift_ib_itg_records = read_redshift_to_df(configs) \
 # COMMAND ----------
 
 redshift_ib_itg_records.show()
+
+# COMMAND ----------
+
+itg_ib = redshift_ib_itg_records
+itg_ib.createOrReplaceTempView('itg_ib')
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC SELECT 'itg' as record, cal_date, country_alpha2, platform_subset, customer_engagement, measure, units from itg_ib
+# MAGIC UNION ALL
+# MAGIC SELECT 'prod' as record, cal_date, country_alpha2, platform_subset, customer_engagement, measure, units from prod_ib
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC 
+# MAGIC SELECT 
+# MAGIC   a.cal_date, 
+# MAGIC   a.country_alpha2, 
+# MAGIC   a.platform_subset, 
+# MAGIC   a.customer_engagement, 
+# MAGIC   a.measure, 
+# MAGIC   a.units,
+# MAGIC   b.units,
+# MAGIC   a.units - b.units
+# MAGIC from itg_ib a 
+# MAGIC left join prod_ib b on 
+# MAGIC   a.cal_date = b.cal_date 
+# MAGIC   and a.country_alpha2 = b.country_alpha2 
+# MAGIC   and a.platform_subset = b.platform_subset 
+# MAGIC   and a.customer_engagement = b.customer_engagement 
+# MAGIC   and a.measure = b.measure
+# MAGIC where a.units <> b.units
+# MAGIC order by 8
 
 # COMMAND ----------
 
