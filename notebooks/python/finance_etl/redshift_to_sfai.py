03c945e62d41249f3f671bb475875c016ad94d7e
candace.cox@hp.com <candace.cox@hp.com>
2022-09-29 23:30:38 +0000

    finance etl update

--- notebooks/python/finance_etl/redshift_to_sfai.py
+++ notebooks/python/finance_etl/redshift_to_sfai.py
@@ -25,23 +25,84 @@
 #--------- fin_prod ---------
 actuals_supplies_baseprod = read_redshift_to_df(configs) \
     .option("dbtable", "fin_prod.actuals_supplies_baseprod") \
-    .load()
+    .load()\
+    .select(col("record")
+      ,col("cal_date")
+      ,col("country_alpha2")
+      ,col("market10")
+      ,col("platform_subset")
+      ,col("base_product_number")
+      ,col("pl")
+      ,col("l5_description").alias("L5_Description")
+      ,col("customer_engagement")
+      ,col("gross_revenue")
+      ,col("net_currency")
+      ,col("contractual_discounts")
+      ,col("discretionary_discounts")
+      ,col("net_revenue")
+      ,col("total_cos")
+      ,col("gross_profit")
+      ,col("revenue_units")
+      ,col("equivalent_units")
+      ,col("yield_x_units")
+      ,col("yield_x_units_black_only")
+      ,col("official")
+      ,col("load_date")
+      ,col("version")
+      ,col("warranty")
+      ,col("other_cos"))
 
 odw_report_rac_product_financials_actuals = read_redshift_to_df(configs) \
     .option("dbtable", "fin_prod.odw_report_rac_product_financials_actuals") \
-    .load()
+    .load()\
+    .select(col("fiscal_year_period").alias("Fiscal Year/Period")
+      , col("material_number").alias("Material Number")
+      , col("profit_center_code").alias("Profit Center Code")
+      , col("segment_code").alias("Segment Code")
+      , col("segment_name").alias("Segment Name")
+      , col("gross_trade_revenues_usd").alias("Gross Trade Revenues USD")
+      , col("contractual_discounts_usd").alias("Contractual Discounts USD")
+      , col("discretionary_discounts_usd").alias("Discretionary Discounts USD")
+      , col("net_currency_usd").alias("Net Currency USD")
+      , col("net_revenues_usd").alias("Net Revenues USD")
+      , col("warr").alias("WARR")
+      , col("total_cost_of_sales_usd").alias("TOTAL COST OF SALES USD")
+      , col("gross_margin_usd").alias("GROSS MARGIN USD")
+      , col("load_date").alias("load_date"))
 
-odw_report_ships_deliveries_actuals = read_redshift_to_df(configs) \
-    .option("dbtable", "fin_prod.odw_report_ships_deliveries_actuals") \
-    .load()
+#odw_report_ships_deliveries_actuals = read_redshift_to_df(configs) \
+#    .option("dbtable", "fin_prod.odw_report_ships_deliveries_actuals") \
+#    .load()
 
 odw_revenue_units_sales_actuals_prelim = read_redshift_to_df(configs) \
     .option("dbtable", "fin_prod.odw_revenue_units_sales_actuals_prelim") \
-    .load()
+    .load()\
+    .select(col("fiscal_year_period").alias("Fiscal Year/Period")
+        , col("profit_center_hier_desc_level4").alias("Profit Center Hier Desc Level4")
+        , col("segment_hier_desc_level4").alias(S"egment Hier Desc Level4")
+        , col("segment_code").alias("Segment Code")
+        , col("segment_name").alias("Segment Name")
+        , col("profit_center_code").alias("Profit Center Code")
+        , col("material_number").alias("Material Number")
+        , col("unit_quantity_sign_flip").alias("Unit Quantity (Sign-Flip)")
+        , col("load_date").alias("load_date")
+        , col("unit_reporting_code").alias("Unit Reporting Code")
+        , col("unit_reporting_description").alias("Unit Reporting Description"))
 
 odw_revenue_units_sales_actuals = read_redshift_to_df(configs) \
     .option("dbtable", "fin_prod.odw_revenue_units_sales_actuals") \
-    .load()
+    .load()\
+    .select(col("fiscal_year_period").alias("Fiscal Year/Period")
+        , col("profit_center_hier_desc_level4").alias("Profit Center Hier Desc Level4")
+        , col("segment_hier_desc_level4").alias(S"egment Hier Desc Level4")
+        , col("segment_code").alias("Segment Code")
+        , col("segment_name").alias("Segment Name")
+        , col("profit_center_code").alias("Profit Center Code")
+        , col("material_number").alias("Material Number")
+        , col("unit_quantity_sign_flip").alias("Unit Quantity (Sign-Flip)")
+        , col("load_date").alias("load_date")
+        , col("unit_reporting_code").alias("Unit Reporting Code")
+        , col("unit_reporting_description").alias("Unit Reporting Description"))
 
 odw_sacp_actuals = read_redshift_to_df(configs) \
     .option("dbtable", "fin_prod.odw_sacp_actuals") \
@@ -49,14 +110,50 @@ odw_sacp_actuals = read_redshift_to_df(configs) \
 
 actuals_supplies_salesprod = read_redshift_to_df(configs) \
     .option("dbtable", "fin_prod.actuals_supplies_salesprod") \
-    .load()
+    .load()\
+    .select(col("record")
+      , col("cal_date")
+      , col("country_alpha2")
+      , col("currency")
+      , col("market10")
+      , col("sales_product_number")
+      , col("pl")
+      , col("l5_description").alias("L5_Description")
+      , col("customer_engagement")
+      , col("gross_revenue")
+      , col("net_currency")
+      , col("contractual_discounts")
+      , col("discretionary_discounts")
+      , col("net_revenue")
+      , col("total_cos")
+      , col("gross_profit")
+      , col("revenue_units")
+      , col("official")
+      , col("load_date")
+      , col("version")
+      , col("warranty")
+      , col("other_cos"))
 
 #---------   mdm  -----------
     
 #---------   prod   ---------
 actuals_supplies = read_redshift_to_df(configs) \
     .option("dbtable", "prod.actuals_supplies") \
-    .load()
+    .load()\
+    .select("record"
+      , "cal_date"
+      , "country_alpha2"
+      , "market10"
+      , "platform_subset"
+      , "base_product_number"
+      , "customer_engagement"
+      , "base_quantity"
+      , "equivalent_units"
+      , "yield_x_units"
+      , "yield_x_units_black_only"
+      , "official"
+      , "load_date"
+      , "version")
 
 
 # COMMAND ----------
