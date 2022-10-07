01514c04218206499c736c54b9654b70e1c29a0a
Matt Koson <matt.koson@hp.com>
2022-10-07 08:35:43 -0700

    Update currency_hedge.py

--- notebooks/python/currency_hedge/currency_hedge.py
+++ notebooks/python/currency_hedge/currency_hedge.py
@@ -7,6 +7,13 @@
 
 # COMMAND ----------
 
+import boto3
+
+from datetime import datetime
+from pyspark.sql.functions import *
+
+# COMMAND ----------
+
 dbutils.widgets.text("spreadsheet_startdate", "")
 
 # COMMAND ----------
@@ -20,19 +27,13 @@ dbutils.widgets.text("spreadsheet_startdate", "")
 # COMMAND ----------
 
 def get_dir_content(ls_path):
-  dir_paths = dbutils.fs.ls(ls_path)
-  subdir_paths = [get_dir_content(p.path) for p in dir_paths if p.isDir() and p.path != ls_path]
-  flat_subdir_paths = [p for subdir in subdir_paths for p in subdir]
-  return list(map(lambda p: p.path, dir_paths)) + flat_subdir_paths
+    dir_paths = dbutils.fs.ls(ls_path)
+    subdir_paths = [get_dir_content(p.path) for p in dir_paths if p.isDir() and p.path != ls_path]
+    flat_subdir_paths = [p for subdir in subdir_paths for p in subdir]
+    return list(map(lambda p: p.path, dir_paths)) + flat_subdir_paths
 
-paths = get_dir_content('s3://dataos-core-prod-team-phoenix/landing/currency_hedge/')
-#[print(p) for p in paths]
-for p in paths:
-  latest_file = p
-
-#  latest_file = latest_file.split("/")[len(latest_file.split("/"))-1]
-#  print(latest_file)
-
+paths = get_dir_content(constants["S3_BASE_BUCKET"][stack] + landing/currency_hedge/')
+latest_file = paths[len(paths)-1]
 
 # COMMAND ----------
 
@@ -49,24 +50,23 @@ sample1Address = "'Revenue Currency Hedge'!A2"
 
 #read excelfile
 sample1DF = spark.read.format("com.crealytics.spark.excel") \
-  .option("header", isHeaderOn) \
-  .option("inferSchema", isInferSchemaOn) \
-  .option("treatEmptyValuesAsNulls", "false") \
-  .option("dataAddress", sample1Address) \
-  .load(sampleDataFilePath)
+    .option("header", isHeaderOn) \
+    .option("inferSchema", isInferSchemaOn) \
+    .option("treatEmptyValuesAsNulls", "false") \
+    .option("dataAddress", sample1Address) \
+    .load(sampleDataFilePath)
 
 # COMMAND ----------
 
 # create dataframe of excel data
-from datetime import datetime
+
 today = datetime.today()
 today_as_string = f"{str(today.year)[2:4]}-{today.month}" if dbutils.widgets.get("spreadsheet_startdate") == "" else dbutils.widgets.get("spreadsheet_startdate")
 
 sample1DF = sample1DF \
-  .withColumnRenamed("Product Category", "product_category") \
-  .withColumnRenamed("Currency", "currency") \
-  .withColumnRenamed(sample1DF.columns[2], today_as_string) # messed up column will always be 3rd position and be current month and year e.g. Oct 
-#display(sample1DF)
+    .withColumnRenamed("Product Category", "product_category") \
+    .withColumnRenamed("Currency", "currency") \
+    .withColumnRenamed(sample1DF.columns[2], today_as_string) # messed up column will always be 3rd position and be current month and year e.g. Oct 
 
 # COMMAND ----------
 
@@ -83,20 +83,18 @@ months_list = list(filtered_sample1DF.columns[2:len(filtered_sample1DF.columns)]
 # COMMAND ----------
 
 # Unpivot the data
-from pyspark.sql.functions import *
 
 # unpivot
 unpivotExpr = f"stack({len(months_list)}"
 
 for month in months_list:
   unpivotExpr = unpivotExpr + f", '{month}', `{month}`"
+
 unpivotExpr = unpivotExpr + ") as (month,revenue_currency_hedge)"
 
 unpivotDF = filtered_sample1DF \
-  .select("product_category", "currency", expr(unpivotExpr)) \
-  .select("product_category", "currency", to_date(col("month"),"yy-MM").alias("month"), "revenue_currency_hedge")
-
-unpivotDF.display()
+    .select("product_category", "currency", expr(unpivotExpr)) \
+    .select("product_category", "currency", to_date(col("month"),"yy-MM").alias("month"), "revenue_currency_hedge")
 
 # COMMAND ----------
 
@@ -111,13 +109,10 @@ max_load_date = str(max_info[1])
 unpivotDF_records = unpivotDF \
     .withColumn("load_date", lit(max_load_date).cast("timestamp")) \
     .withColumn("version", lit(max_version)) \
-    .withColumn("revenue_currency_hedge",col("revenue_currency_hedge").cast("double"))
-
-unpivotDF_records = unpivotDF_records.filter(unpivotDF_records.product_category.isNotNull())
-unpivotDF_records = unpivotDF_records.filter(unpivotDF_records.revenue_currency_hedge.isNotNull())
-unpivotDF_records = unpivotDF_records.filter("revenue_currency_hedge <> 0")
-
-# unpivotDF_records.display()
+    .withColumn("revenue_currency_hedge",col("revenue_currency_hedge").cast("double")) \
+    .filter(unpivotDF_records.product_category.isNotNull()) \
+    .filter(unpivotDF_records.revenue_currency_hedge.isNotNull()) \
+    .filter("revenue_currency_hedge <> 0")
 
 # COMMAND ----------
 
@@ -135,17 +130,14 @@ s3_output_bucket = constants["S3_BASE_BUCKET"][stack] + "product/currency_hedge/
 # write the data out in parquet format
 write_df_to_s3(unpivotDF_records, s3_output_bucket, "parquet", "overwrite")
 
-
 # COMMAND ----------
 
-
 # # Rename the columns to match SFAI
-unpivotDF_records = unpivotDF_records.withColumnRenamed("product_category","Product_Category")
-unpivotDF_records = unpivotDF_records.withColumnRenamed("currency","Currency")
-unpivotDF_records = unpivotDF_records.withColumnRenamed("month","Month")
-unpivotDF_records = unpivotDF_records.withColumnRenamed("revenue_currency_hedge","Revenue_Currency_Hedge")
-
-# display(unpivotDF_records)
+unpivotDF_records = unpivotDF_records \
+    .withColumnRenamed("product_category","Product_Category") \
+    .withColumnRenamed("currency","Currency") \
+    .withColumnRenamed("month","Month") \
+    .withColumnRenamed("revenue_currency_hedge","Revenue_Currency_Hedge")
 
 # COMMAND ----------
 
@@ -156,22 +148,10 @@ write_df_to_sqlserver(configs, unpivotDF_records, "IE2_Prod.dbo.currency_hedge",
 
 # copy the file from the landing bucket to an archive bucket, then delete from the landing bucket
 
-import boto3
-
 working_file = latest_file.split("/")[len(latest_file.split("/"))-1]
 file_to_delete = 'landing/currency_hedge/' + working_file
 new_location = 'archive/currency_hedge/' + working_file
 
-s3_client = boto3.client('s3')
-
 s3 = boto3.resource('s3')
-# s3.Object('dataos-core-dev-team-phoenix', new_location).copy_from(CopySource='dataos-core-dev-team-phoenix/' + file_to_delete)
-# s3.Object('dataos-core-dev-team-phoenix',file_to_delete).delete()
-
-s3.Object('dataos-core-prod-team-phoenix', new_location).copy_from(CopySource='dataos-core-prod-team-phoenix/' + file_to_delete)
+s3.Object('dataos-core-prod-team-phoenix', new_location).copy_from(CopySource=f'dataos-core-{stack}-team-phoenix/' + file_to_delete)
 s3.Object('dataos-core-prod-team-phoenix',file_to_delete).delete()
-
-
-# COMMAND ----------
-
-
