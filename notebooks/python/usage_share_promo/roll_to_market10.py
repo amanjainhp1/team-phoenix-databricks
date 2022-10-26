94a2ba716f7678d69e9b82a7351d955b68750629
Brent Timothy <brent.timothy@hp.com>
2022-10-26 08:00:47 -0600

    Update roll_to_market10.py

--- notebooks/python/usage_share_promo/roll_to_market10.py
+++ notebooks/python/usage_share_promo/roll_to_market10.py
@@ -36,12 +36,13 @@ country_info.createOrReplaceTempView("country_info")
 # COMMAND ----------
 
 # Read in U/S data
-us_table = spark.read.parquet(f"{constants['S3_BASE_BUCKET'][stack]}spectrum/demand/{datestamp}/*")
+us_table = spark.read.parquet(f"{constants['S3_BASE_BUCKET'][stack]}spectrum/demand/{datestamp}.1/*")
 us_table.createOrReplaceTempView("us_table")
 
 # COMMAND ----------
 
 #roll up to market10 -- changing source to % of source, need to find way to roll up other notes (forecast process note, proxy used)
+
 convert = f"""
 with step1 as (
     SELECT us.cal_date
@@ -49,62 +50,62 @@ with step1 as (
 	, us.platform_subset
 	, us.customer_engagement
     --usage
-	, SUM(CASE WHEN us.measure='USAGE' AND us.data_source='TELEMETRY' THEN 1
+	, SUM(CASE WHEN us.measure='USAGE' AND us.source='TELEMETRY' THEN 1
 		ELSE 0 END) AS data_source_u
-    , SUM(CASE WHEN us.measure='USAGE' AND us.data_source='NPI' THEN 1
+    , SUM(CASE WHEN us.measure='USAGE' AND us.source='NPI' THEN 1
 		ELSE 0 END) AS data_source_u_n
-    , SUM(CASE WHEN us.measure='USAGE' AND us.data_source='MATURE' THEN 1
+    , SUM(CASE WHEN us.measure='USAGE' AND us.source='MATURE' THEN 1
 		ELSE 0 END) AS data_source_u_o
-    , SUM(CASE WHEN us.measure='USAGE' AND us.data_source='MODELED' THEN 1
+    , SUM(CASE WHEN us.measure='USAGE' AND us.source='MODELED' THEN 1
 		ELSE 0 END) AS data_source_u_m
-    , SUM(CASE WHEN us.measure='USAGE' AND us.data_source='WORKING-FORECAST OVERRIDE' THEN 1
+    , SUM(CASE WHEN us.measure='USAGE' AND us.source='WORKING-FORECAST OVERRIDE' THEN 1
 		ELSE 0 END) AS data_source_u_w
-    , SUM(CASE WHEN us.measure='USAGE' AND us.data_source='EPA-DRIVERS OVERRIDE' THEN 1
+    , SUM(CASE WHEN us.measure='USAGE' AND us.source='EPA-DRIVERS OVERRIDE' THEN 1
 		ELSE 0 END) AS data_source_u_e	
     --color usage
-	, SUM(CASE WHEN us.measure='COLOR_USAGE'AND us.data_source='TELEMETRY' THEN 1
+	, SUM(CASE WHEN us.measure='COLOR_USAGE'AND us.source='TELEMETRY' THEN 1
 		ELSE 0 END) AS data_source_c
-    , SUM(CASE WHEN us.measure='COLOR_USAGE' AND us.data_source='NPI' THEN 1
+    , SUM(CASE WHEN us.measure='COLOR_USAGE' AND us.source='NPI' THEN 1
 		ELSE 0 END) AS data_source_c_n
-    , SUM(CASE WHEN us.measure='COLOR_USAGE' AND us.data_source='MATURE' THEN 1
+    , SUM(CASE WHEN us.measure='COLOR_USAGE' AND us.source='MATURE' THEN 1
 		ELSE 0 END) AS data_source_c_o
-    , SUM(CASE WHEN us.measure='COLOR_USAGE' AND us.data_source='MODELED' THEN 1
+    , SUM(CASE WHEN us.measure='COLOR_USAGE' AND us.source='MODELED' THEN 1
 		ELSE 0 END) AS data_source_c_m
-    , SUM(CASE WHEN us.measure='COLOR_USAGE' AND us.data_source='WORKING-FORECAST OVERRIDE' THEN 1
+    , SUM(CASE WHEN us.measure='COLOR_USAGE' AND us.source='WORKING-FORECAST OVERRIDE' THEN 1
 		ELSE 0 END) AS data_source_c_w
-    , SUM(CASE WHEN us.measure='COLOR_USAGE' AND us.data_source='EPA-DRIVERS OVERRIDE' THEN 1
+    , SUM(CASE WHEN us.measure='COLOR_USAGE' AND us.source='EPA-DRIVERS OVERRIDE' THEN 1
 		ELSE 0 END) AS data_source_c_e	
     --black usage
-	, SUM(CASE WHEN us.measure='K_USAGE' AND us.data_source='TELEMETRY' THEN 1
+	, SUM(CASE WHEN us.measure='K_USAGE' AND us.source='TELEMETRY' THEN 1
 		ELSE 0 END) AS data_source_k
-    , SUM(CASE WHEN us.measure='K_USAGE' AND us.data_source='NPI' THEN 1
+    , SUM(CASE WHEN us.measure='K_USAGE' AND us.source='NPI' THEN 1
 		ELSE 0 END) AS data_source_k_n
-    , SUM(CASE WHEN us.measure='K_USAGE' AND us.data_source='MATURE' THEN 1
+    , SUM(CASE WHEN us.measure='K_USAGE' AND us.source='MATURE' THEN 1
 		ELSE 0 END) AS data_source_k_o
-    , SUM(CASE WHEN us.measure='K_USAGE' AND us.data_source='MODELED' THEN 1
+    , SUM(CASE WHEN us.measure='K_USAGE' AND us.source='MODELED' THEN 1
 		ELSE 0 END) AS data_source_k_m
-    , SUM(CASE WHEN us.measure='K_USAGE' AND us.data_source='WORKING-FORECAST OVERRIDE' THEN 1
+    , SUM(CASE WHEN us.measure='K_USAGE' AND us.source='WORKING-FORECAST OVERRIDE' THEN 1
 		ELSE 0 END) AS data_source_k_w
-    , SUM(CASE WHEN us.measure='K_USAGE' AND us.data_source='EPA-DRIVERS OVERRIDE' THEN 1
+    , SUM(CASE WHEN us.measure='K_USAGE' AND us.source='EPA-DRIVERS OVERRIDE' THEN 1
 		ELSE 0 END) AS data_source_k_e		
     --share     
-	, SUM(CASE WHEN us.measure='USAGE' AND us.data_source='TELEMETRY' AND us.customer_engagement='I-INK' THEN 1
-        WHEN us.measure='HP_SHARE' AND us.data_source='TELEMETRY' AND us.customer_engagement !='I-INK' THEN 1
+	, SUM(CASE WHEN us.measure='USAGE' AND us.source='TELEMETRY' AND us.customer_engagement='I-INK' THEN 1
+        WHEN us.measure='HP_SHARE' AND us.source='TELEMETRY' AND us.customer_engagement !='I-INK' THEN 1
 		ELSE 0 END) AS data_source_s
-    , SUM(CASE WHEN us.measure='USAGE' AND us.data_source='NPI' AND us.customer_engagement='I-INK' THEN 1
-        WHEN us.measure='HP_SHARE' AND us.data_source='NPI' AND us.customer_engagement !='I-INK' THEN 1
+    , SUM(CASE WHEN us.measure='USAGE' AND us.source='NPI' AND us.customer_engagement='I-INK' THEN 1
+        WHEN us.measure='HP_SHARE' AND us.source='NPI' AND us.customer_engagement !='I-INK' THEN 1
 		ELSE 0 END) AS data_source_s_n
-    , SUM(CASE WHEN us.measure='USAGE' AND us.data_source='MATURE' AND us.customer_engagement='I-INK' THEN 1
-        WHEN us.measure='HP_SHARE' AND us.data_source='MATURE' AND us.customer_engagement !='I-INK' THEN 1
+    , SUM(CASE WHEN us.measure='USAGE' AND us.source='MATURE' AND us.customer_engagement='I-INK' THEN 1
+        WHEN us.measure='HP_SHARE' AND us.source='MATURE' AND us.customer_engagement !='I-INK' THEN 1
 		ELSE 0 END) AS data_source_s_o
-    , SUM(CASE WHEN us.measure='USAGE' AND us.data_source='MODELED' AND us.customer_engagement='I-INK' THEN 1
-        WHEN us.measure='HP_SHARE' AND us.data_source='MODELED' AND us.customer_engagement !='I-INK' THEN 1
+    , SUM(CASE WHEN us.measure='USAGE' AND us.source='MODELED' AND us.customer_engagement='I-INK' THEN 1
+        WHEN us.measure='HP_SHARE' AND us.source='MODELED' AND us.customer_engagement !='I-INK' THEN 1
 		ELSE 0 END) AS data_source_s_m
-    , SUM(CASE WHEN us.measure='USAGE' AND us.data_source='WORKING-FORECAST OVERRIDE' AND us.customer_engagement='I-INK' THEN 1
-        WHEN us.measure='HP_SHARE' AND us.data_source='WORKING-FORECAST OVERRIDE' AND us.customer_engagement !='I-INK' THEN 1
+    , SUM(CASE WHEN us.measure='USAGE' AND us.source='WORKING-FORECAST OVERRIDE' AND us.customer_engagement='I-INK' THEN 1
+        WHEN us.measure='HP_SHARE' AND us.source='WORKING-FORECAST OVERRIDE' AND us.customer_engagement !='I-INK' THEN 1
 		ELSE 0 END) AS data_source_s_w
-    , SUM(CASE WHEN us.measure='USAGE' AND us.data_source='EPA-DRIVERS OVERRIDE' AND us.customer_engagement='I-INK' THEN 1
-        WHEN us.measure='HP_SHARE' AND us.data_source='EPA-DRIVERS OVERRIDE' AND us.customer_engagement !='I-INK' THEN 1
+    , SUM(CASE WHEN us.measure='USAGE' AND us.source='EPA-DRIVERS OVERRIDE' AND us.customer_engagement='I-INK' THEN 1
+        WHEN us.measure='HP_SHARE' AND us.source='EPA-DRIVERS OVERRIDE' AND us.customer_engagement !='I-INK' THEN 1
 		ELSE 0 END) AS data_source_s_e
 		
     , SUM(CASE WHEN us.measure='USAGE' THEN us.units ELSE 0 END) AS usage
@@ -193,8 +194,6 @@ GROUP BY
       ,SUM(u.data_source_c*coalesce(ib,0)) as data_source_c
       ,SUM(u.data_source_k*coalesce(ib,0)) as data_source_k
       ,SUM(u.data_source_s*coalesce(ib,0)) as data_source_s
-      ,SUM(u.data_source_w*coalesce(ib,0)) as data_source_w
-      ,SUM(u.data_source_e*coalesce(ib,0)) as data_source_e
       ,SUM(u.data_source_u_n*coalesce(ib,0)) as data_source_u_n
       ,SUM(u.data_source_c_n*coalesce(ib,0)) as data_source_c_n
       ,SUM(u.data_source_k_n*coalesce(ib,0)) as data_source_k_n
@@ -219,7 +218,6 @@ GROUP BY
       ,SUM(u.hp_pages) AS hp_pages
       ,SUM(u.total_color_pages) AS color_pages
       ,SUM(u.total_k_pages) AS black_pages
-      ,SUM(u.hp_pages) AS hp_pages
       ,SUM(u.non_hp_pages) AS non_hp_pages
       ,SUM(u.non_hp_color_pages) AS non_hp_color_pages
       ,SUM(u.non_hp_k_pages) AS non_hp_k_pages
@@ -242,8 +240,6 @@ GROUP BY
       ,SUM(u.data_source_c) as data_source_c
       ,SUM(u.data_source_k) as data_source_k
       ,SUM(u.data_source_s) as data_source_s
-      ,SUM(u.data_source_w) as data_source_w
-      ,SUM(u.data_source_e) as data_source_e
       ,SUM(u.data_source_u_n) as data_source_u_n
       ,SUM(u.data_source_c_n) as data_source_c_n
       ,SUM(u.data_source_k_n) as data_source_k_n
@@ -290,8 +286,6 @@ GROUP BY
 	, h4.data_source_s/nullif(ib,0) as data_source_s
 	, h4.data_source_c/nullif(ib,0) as data_source_c
 	, h4.data_source_k/nullif(ib,0) as data_source_k
-	, h4.data_source_w/nullif(ib,0) as data_source_w
-	, h4.data_source_e/nullif(ib,0) as data_source_e
     , h4.data_source_u_n/nullif(ib,0) as data_source_u_n
 	, h4.data_source_s_n/nullif(ib,0) as data_source_s_n
 	, h4.data_source_c_n/nullif(ib,0) as data_source_c_n
@@ -328,6 +322,7 @@ GROUP BY
       	, h4.non_hp_k_pages AS non_hp_k_pages
       	, h4.hp_k_pages AS hp_k_pages
       	, h4.hp_color_pages AS hp_color_pages
+		, h4.ib
 FROM step4 h4
 
 ), step6 as (
@@ -338,7 +333,6 @@ SELECT cal_date
 	, 'USAGE' as measure
 	, usage as units
 	, CONCAT(round(data_source_u*100,0),"% T, ",round(data_source_u_n*100,0),"% N, ",round(data_source_u_o*100,0),"% MA, ",round(data_source_u_m*100,0),"% MD",round(data_source_u_w*100,0),"% WF",round(data_source_u_e*100,0),"% EP") as source
-    , round(data_source_u+data_source_u_n+data_source_u_o+data_source_u_m+data_source_u_w+data_source_u_e,1) as test_src
 FROM step5
 WHERE usage IS NOT NULL
     AND usage > 0
@@ -350,7 +344,6 @@ SELECT cal_date
 	, 'HP_SHARE' as measure
 	, page_share as units
 	, CONCAT(round(data_source_s*100,0),"% T, ",round(data_source_s_n*100,0),"% N, ",round(data_source_s_o*100,0),"% MA, ",round(data_source_s_m*100,0),"% MD",round(data_source_s_w*100,0),"% WF",round(data_source_s_e*100,0),"% EP") as source
-    , round(data_source_s+data_source_s_n+data_source_s_o+data_source_s_m+data_source_s_w+data_source_s_e,1) as test_src
 FROM step5
 WHERE page_share IS NOT NULL
     AND page_share > 0
@@ -362,7 +355,6 @@ SELECT cal_date
 	, 'COLOR_USAGE' as measure
 	, usage_c as units
 	, CONCAT(round(data_source_c*100,0),"% T, ",round(data_source_c_n*100,0),"% N, ",round(data_source_c_o*100,0),"% MA, ",round(data_source_c_m*100,0),"% MD",round(data_source_c_w*100,0),"% WF",round(data_source_c_e*100,0),"% EP") as source
-    , round(data_source_c+data_source_c_n+data_source_c_o+data_source_c_m+data_source_c_w+data_source_c_e,1) as test_src
 FROM step5
 WHERE usage_c IS NOT NULL
     AND usage_c > 0
@@ -374,7 +366,6 @@ SELECT cal_date
 	, 'K_USAGE' as measure
 	, usage_k as units
 	, CONCAT(round(data_source_k*100,0),"% T, ",round(data_source_k_n*100,0),"% N, ",round(data_source_k_o*100,0),"% MA, ",round(data_source_k_m*100,0),"% MD",round(data_source_k_w*100,0),"% WF",round(data_source_k_e*100,0),"% EP") as source
-    , round(data_source_k+data_source_k_n+data_source_k_o+data_source_k_m+data_source_k_w+data_source_k_e,1) as test_src
 FROM step5
 WHERE usage_k IS NOT NULL
     AND usage_k > 0
@@ -386,7 +377,6 @@ SELECT cal_date
 	, 'TOTAL_PAGES' as measure
 	, total_pages as units
 	, CONCAT(round(data_source_u*100,0),"% T, ",round(data_source_u_n*100,0),"% N, ",round(data_source_u_o*100,0),"% MA, ",round(data_source_u_m*100,0),"% MD",round(data_source_k_w*100,0),"% WF",round(data_source_k_e*100,0),"% EP") as source
-    , round(data_source_u+data_source_u_n+data_source_u_o+data_source_u_m+data_source_u_w+data_source_u_e,1) as test_src
 FROM step5
 WHERE total_pages IS NOT NULL
     AND total_pages > 0
@@ -398,7 +388,6 @@ SELECT cal_date
 	, 'TOTAL_COLOR_PAGES' as measure
 	, total_color_pages as units
 	, CONCAT(round(data_source_u*100,0),"% T, ",round(data_source_u_n*100,0),"% N, ",round(data_source_u_o*100,0),"% MA, ",round(data_source_u_m*100,0),"% MD",round(data_source_k_w*100,0),"% WF",round(data_source_k_e*100,0),"% EP") as source
-    , round(data_source_u+data_source_u_n+data_source_u_o+data_source_u_m+data_source_u_w+data_source_u_e,1) as test_src
 FROM step5
 WHERE total_color_pages IS NOT NULL
     AND total_color_pages > 0
@@ -410,7 +399,6 @@ SELECT cal_date
 	, 'TOTAL_K_PAGES' as measure
 	, total_k_pages as units
 	, CONCAT(round(data_source_u*100,0),"% T, ",round(data_source_u_n*100,0),"% N, ",round(data_source_u_o*100,0),"% MA, ",round(data_source_u_m*100,0),"% MD",round(data_source_k_w*100,0),"% WF",round(data_source_k_e*100,0),"% EP") as source
-    , round(data_source_u+data_source_u_n+data_source_u_o+data_source_u_m+data_source_u_w+data_source_u_e,1) as test_src
 FROM step5
 WHERE total_k_pages IS NOT NULL
     AND total_k_pages > 0
@@ -422,7 +410,6 @@ SELECT cal_date
 	, 'HP_PAGES' as measure
 	, hp_pages as units
 	, CONCAT(round(data_source_u*100,0),"% T, ",round(data_source_u_n*100,0),"% N, ",round(data_source_u_o*100,0),"% MA, ",round(data_source_u_m*100,0),"% MD",round(data_source_k_w*100,0),"% WF",round(data_source_k_e*100,0),"% EP") as source
-    , round(data_source_u+data_source_u_n+data_source_u_o+data_source_u_m+data_source_u_w+data_source_u_e,1) as test_src
 FROM step5
 WHERE hp_pages IS NOT NULL
     AND hp_pages > 0
@@ -434,7 +421,6 @@ SELECT cal_date
 	, 'NON_HP_PAGES' as measure
 	, non_hp_pages as units
 	, CONCAT(round(data_source_u*100,0),"% T, ",round(data_source_u_n*100,0),"% N, ",round(data_source_u_o*100,0),"% MA, ",round(data_source_u_m*100,0),"% MD",round(data_source_k_w*100,0),"% WF",round(data_source_k_e*100,0),"% EP") as source
-    , round(data_source_u+data_source_u_n+data_source_u_o+data_source_u_m+data_source_u_w+data_source_u_e,1) as test_src
 FROM step5
 WHERE non_hp_pages IS NOT NULL
     AND non_hp_pages > 0
@@ -446,7 +432,6 @@ SELECT cal_date
 	, 'NON_HP_K_PAGES' as measure
 	, non_hp_k_pages as units
 	, CONCAT(round(data_source_u*100,0),"% T, ",round(data_source_u_n*100,0),"% N, ",round(data_source_u_o*100,0),"% MA, ",round(data_source_u_m*100,0),"% MD",round(data_source_k_w*100,0),"% WF",round(data_source_k_e*100,0),"% EP") as source
-    , round(data_source_u+data_source_u_n+data_source_u_o+data_source_u_m+data_source_u_w+data_source_u_e,1) as test_src
 FROM step5
 WHERE non_hp_k_pages IS NOT NULL
     AND non_hp_k_pages > 0
@@ -458,7 +443,6 @@ SELECT cal_date
 	, 'NON_HP_COLOR_PAGES' as measure
 	, non_hp_color_pages as units
 	, CONCAT(round(data_source_u*100,0),"% T, ",round(data_source_u_n*100,0),"% N, ",round(data_source_u_o*100,0),"% MA, ",round(data_source_u_m*100,0),"% MD",round(data_source_k_w*100,0),"% WF",round(data_source_k_e*100,0),"% EP") as source
-    , round(data_source_u+data_source_u_n+data_source_u_o+data_source_u_m+data_source_u_w+data_source_u_e,1) as test_src
 FROM step5
 WHERE non_hp_color_pages IS NOT NULL
     AND non_hp_color_pages > 0
@@ -470,7 +454,6 @@ SELECT cal_date
 	, 'HP_K_PAGES' as measure
 	, hp_k_pages as units
 	, CONCAT(round(data_source_u*100,0),"% T, ",round(data_source_u_n*100,0),"% N, ",round(data_source_u_o*100,0),"% MA, ",round(data_source_u_m*100,0),"% MD",round(data_source_k_w*100,0),"% WF",round(data_source_k_e*100,0),"% EP") as source
-    , round(data_source_u+data_source_u_n+data_source_u_o+data_source_u_m+data_source_u_w+data_source_u_e,1) as test_src
 FROM step5
 WHERE hp_k_pages IS NOT NULL
     AND hp_k_pages > 0
@@ -482,7 +465,6 @@ SELECT cal_date
 	, 'HP_COLOR_PAGES' as measure
 	, hp_color_pages as units
 	, CONCAT(round(data_source_u*100,0),"% T, ",round(data_source_u_n*100,0),"% N, ",round(data_source_u_o*100,0),"% MA, ",round(data_source_u_m*100,0),"% MD",round(data_source_k_w*100,0),"% WF",round(data_source_k_e*100,0),"% EP") as source
-    , round(data_source_u+data_source_u_n+data_source_u_o+data_source_u_m+data_source_u_w+data_source_u_e,1) as test_src
 FROM step5
 WHERE hp_color_pages IS NOT NULL
     AND hp_color_pages > 0
@@ -493,7 +475,7 @@ SELECT cal_date
 	, customer_engagement
 	, 'IB' as measure
 	, ib as units
-	, 'IB' as test_src
+	, 'IB' as source
 FROM step5
 WHERE ib IS NOT NULL
 )
@@ -507,7 +489,6 @@ WHERE ib IS NOT NULL
       ,units
       ,'{ib_version}' as ib_version
       ,source
-      ,test_src
       ,CONCAT(CAST(current_date() AS DATE),".1") as version
       ,CAST(current_date() AS DATE) AS load_date
       FROM step6)
