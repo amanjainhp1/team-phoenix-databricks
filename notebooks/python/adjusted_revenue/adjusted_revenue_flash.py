1d2a72a5af7e1da866f80af44f250f6ec6e72b3c
noelle.diederich@hp.com <noelle.diederich@hp.com>
2022-12-01 17:11:23 +0000

    fix commit

--- notebooks/python/adjusted_revenue/adjusted_revenue_flash.py
+++ notebooks/python/adjusted_revenue/adjusted_revenue_flash.py
@@ -1282,7 +1282,7 @@ for table in tables:
     renamed_df.write \
       .format(write_format) \
       .mode(mode) \
-      .option("overwriteSchema", "true")\
+      .option("mergeSchema", "true")\
       .save(save_path)
 
     spark.sql(f"CREATE SCHEMA IF NOT EXISTS {schema}")
