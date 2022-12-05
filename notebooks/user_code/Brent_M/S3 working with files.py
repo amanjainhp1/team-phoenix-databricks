781855a6d2d5e6b9006c4dc7bc5154e7dd2f5427
brent.merrick@hp.com <brent.merrick@hp.com>
2022-12-05 15:41:44 +0000

    accounting rates ETL package and tools update

--- notebooks/user_code/Brent_M/S3 working with files.py
+++ notebooks/user_code/Brent_M/S3 working with files.py
@@ -33,3 +33,31 @@ ams_s3 = spark.read.csv(path="s3a://enrich-data-lake-restricted-prod/gpsy/insigh
 # MAGIC %sh
 # MAGIC 
 # MAGIC aws s3 ls s3://hp-bigdata-prod-enrichment/ie2_deliverables/rdma/
+
+# COMMAND ----------
+
+import boto3
+from datetime import datetime
+from pyspark.sql.functions import *
+from pyspark.sql.types import IntegerType
+from pyspark.sql import functions as func
+from pyspark.sql.types import DoubleType
+
+# COMMAND ----------
+
+def retrieve_latest_s3_object_by_prefix(bucket, prefix):
+    s3 = boto3.resource('s3')
+    objects = list(s3.Bucket(bucket).objects.filter(Prefix=prefix))
+    objects.sort(key=lambda o: o.last_modified)
+    return objects[-1].key
+
+#rdma_base_bucket = "hp-bigdata-prod-enrichment"
+gpsy_base_bucket = "enrich-data-lake-restricted-prod"
+
+#rdma_base_to_sales_latest_file = retrieve_latest_s3_object_by_prefix(rdma_base_bucket, "ie2_deliverables/rdma/rdma_base_to_sales")
+#rdma_base_to_sales_latest_file = rdma_base_to_sales_latest_file.split("/")[len(rdma_base_to_sales_latest_file.split("/"))-1]
+#print(rdma_base_to_sales_latest_file)
+
+gpsy_latest_file = retrieve_latest_s3_object_by_prefix(gpsy_base_bucket, "/gpsy")
+gpsy_latest_file = gpsy_latest_file.split("/")[len(gpsy_latest_file.split("/"))-1]
+print(gpsy_latest_file)
--- notebooks/user_code/Brent_M/parquet multiple files read to DF.py
+++ notebooks/user_code/Brent_M/parquet multiple files read to DF.py
@@ -1,20 +1,20 @@
 # Databricks notebook source
-# MAGIC %run ../common/s3_utils
+# MAGIC %run ../../python/common/s3_utils
 
 # COMMAND ----------
 
-# MAGIC %run ../common/database_utils
+# MAGIC %run ../../python/common/database_utils
 
 # COMMAND ----------
 
-# MAGIC %run ../common/configs
+# MAGIC %run ../../python/common/configs
 
 # COMMAND ----------
 
 
 # load parquet file
 # edw_s3 = spark.read.parquet("s3://dataos-core-itg-team-phoenix-fin/landing/EDW/edw_revenue_units_sales_landing/part-00000-tid-3689856782391625736-5511b3d9-c4a5-4228-be00-374f544ef370-0-1-c000.snappy.parquet")
-list_price_gpsy = spark.read.parquet("s3://dataos-core-prod-team-phoenix/product/list_price_gpsy/2022.05.04.1")
+list_price_gpsy = spark.read.parquet("s3://dataos-core-prod-team-phoenix/spectrum/list_price_filtered_historical/2022-11-19/*.parquet")
 
 # list_price_gpsy.count()
 list_price_gpsy.display()
