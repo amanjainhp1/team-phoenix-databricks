b2d99615ffc84a31d7a998653d157b3fd7b433d4
Brent Timothy <brent.timothy@hp.com>
2023-01-20 09:48:13 -0700

    fix sql

--- notebooks/python/usage_share_promo/npi.py
+++ notebooks/python/usage_share_promo/npi.py
@@ -501,18 +501,18 @@ npi_helper_4_ii.createOrReplaceTempView("npi_helper_4_ii")
 
 %sql
 
-npi_helper_4 = """
+
 SELECT * FROM npi_helper_4
 UNION ALL
 SELECT * FROM npi_helper_4_bi
 UNION ALL
 SELECT * FROM npi_helper_4_ii
-"""
+
 #npi_helper_4=spark.sql(npi_helper_4)
 #npi_helper_4.createOrReplaceTempView("npi_helper_4")
 # COMMAND ----------
 
-npi_helper_4 = sqlContext.sql("select * from npi_helper4")
+npi_helper_4 = spark.sql("select * from sql_result")
 npi_helper_4.createOrReplaceTempView("npi_helper_4")
 
 # COMMAND ----------
