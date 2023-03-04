9f7c549b489696b2b0657ad3d307b198220c1380
candace.cox@hp.com <candace.cox@hp.com>
2023-03-04 00:33:23 +0000

    forecasting engine validation updates

--- notebooks/python/toner_forecasting_engine/working_combined_country/01_working_forecast_combined.py
+++ notebooks/python/toner_forecasting_engine/working_combined_country/01_working_forecast_combined.py
@@ -17,41 +17,6 @@
 
 # COMMAND ----------
 
-# ink_working_fcst = read_redshift_to_df(configs) \
-#     .option("query", "select * from prod.working_forecast where version = '{}'".format(ink_ib_version)) \
-#     .load()
-
-# toner_working_fcst = read_redshift_to_df(configs) \
-#     .option("query", "select * from prod.working_forecast where version = '{}'".format(toner_ib_version)) \
-#     .load()
-
-
-ink_working_fcst = read_redshift_to_df(configs) \
-    .option("dbtable", "scen.ink_working_fcst") \
-    .load()
-
-toner_working_fcst = read_redshift_to_df(configs) \
-    .option("dbtable", "scen.toner_working_fcst") \
-    .load()
-
-# COMMAND ----------
-
-tables = [
-    ["ink_working_fcst", ink_working_fcst, "overwrite"],
-    ["toner_working_fcst", toner_working_fcst, "overwrite"]
-]
-
-# COMMAND ----------
-
-for table in tables:
-    df = table[1]
-    table_name = table[0]
-    df.createOrReplaceTempView('{}'.format(table_name))
-    print("row count for " + table_name + ":")
-    print(df.count())
-
-# COMMAND ----------
-
 combined = spark.sql("""
     SELECT 'WORKING_FORECAST_TONER' AS record
         , wft.cal_date
@@ -90,14 +55,6 @@ combined = spark.sql("""
     FROM ink_working_fcst AS wfi
 """)
 
-
-
-
-# COMMAND ----------
-
-combined.printSchema()
-
-# COMMAND ----------
 
 write_df_to_redshift(configs, combined, "scen.working_forecast_combined", "overwrite")
 
