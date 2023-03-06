bb06f0d0fcc3d09970b53c09d2c6048d33abf26c
Matt Koson <matt.koson@hp.com>
2023-03-06 13:40:52 -0800

    Update scenario_mix_rate_sproc.sql

--- redshift/stored_procedures/prod/scenario_mix_rate_sproc.sql
+++ redshift/stored_procedures/prod/scenario_mix_rate_sproc.sql
@@ -86,7 +86,7 @@ CREATE TABLE stage.scenario_mix_rate_staging(
     ,value DOUBLE PRECISION NOT NULL
 );
 GRANT ALL ON TABLE stage.scenario_mix_rate_staging TO auto_glue;
-EXECUTE 'GRANT ALL ON TABLE fin_stage.forecast_contra_input_lt_staging TO GROUP '||group_param||';';
+EXECUTE 'GRANT ALL ON TABLE stage.scenario_mix_rate_staging TO GROUP '||group_param||';';
 
 ---------LOAD STAGING TABLE----------------------------------------------------------
 
