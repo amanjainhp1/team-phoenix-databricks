29bbf15dcf201865d257e721e2c8d9d86f8f0997
noelle.diederich@hp.com <noelle.diederich@hp.com>
2023-03-30 00:02:07 +0000

    update segment hierarchy load

--- notebooks/python/finance_etl/ms4_segment_hierarchy_update.py
+++ notebooks/python/finance_etl/ms4_segment_hierarchy_update.py
@@ -80,3 +80,79 @@ ms4_latest_segment_hierarchy_df = ms4_latest_segment_hierarchy_df.withColumn("of
 ms4_latest_segment_hierarchy_df = ms4_segment_hierarchy_df.union(ms4_latest_segment_hierarchy_df) 
 
 write_df_to_redshift(configs=configs, df=ms4_latest_segment_hierarchy_df, destination="fin_stage.ms4_segment_hierarchy", mode="append", postactions="", preactions="TRUNCATE fin_stage.ms4_segment_hierarchy")
+
+# COMMAND ----------
+
+# load S3 tables to df
+ms4_segment_hierarchy = read_redshift_to_df(configs) \
+    .option("dbtable", "fin_stage.ms4_segment_hierarchy") \
+    .load()
+profit_center_code_xref = read_redshift_to_df(configs) \
+    .option("query", "SELECT * FROM mdm.profit_center_code_xref WHERE record = 'PROFIT_CENTER'") \
+    .load()
+
+# COMMAND ----------
+
+ms4_segment_hierarchy.createOrReplaceTempView("ms4_segment_hierarchy")
+profit_center_code_xref.createOrReplaceTempView("profit_center_code_xref")
+
+# COMMAND ----------
+
+load_segment_to_profit_center_code_xref = """
+SELECT profit_center_id, 
+	record,
+	profit_center_code,
+	profit_center_name,
+	pc_level_0,
+	pc_level_1,
+	pc_level_2,
+	pc_level_3,
+	pc_level_4,
+	pc_level_5,
+	pc_level_6,
+	country_alpha2,
+	region_3,
+	region_5,
+	update_date,
+	official,
+	load_date,
+	version
+FROM profit_center_code_xref
+
+UNION ALL
+
+SELECT '' as profit_center_id,
+	record,
+	profit_center_code,
+	profit_center_name,
+	pc_level_0,
+	pc_level_1,
+	pc_level_2,
+	pc_level_3,
+	pc_level_4,
+	pc_level_5,
+	pc_level_6,
+	country_alpha2,
+	region_3,
+	region_5,
+	CAST(update_date AS date),
+	official,
+	CAST(load_date AS timestamp),
+	version
+FROM ms4_segment_hierarchy
+"""
+
+load_segment_to_profit_center_code_xref = spark.sql(load_segment_to_profit_center_code_xref)
+load_segment_to_profit_center_code_xref.createOrReplaceTempView("load_segment_to_profit_center_code_xref")
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select *
+# MAGIC from load_segment_to_profit_center_code_xref
+# MAGIC where profit_center_code = 'SEMLT00'
+
+# COMMAND ----------
+
+#LOAD TO DB
+write_df_to_redshift(configs, load_segment_to_profit_center_code_xref, "mdm.profit_center_code_xref", "append", postactions = "", preactions = "")
