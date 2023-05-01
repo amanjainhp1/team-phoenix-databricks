1dcdc3517fdb8d02d288b9e1ef9e18e9ed96c837
matt.koson@hp.com <matt.koson@hp.com>
2023-05-01 21:24:02 +0000

    Update working_forecast_promo.py

--- notebooks/python/working_forecast_promo/working_forecast_promo.py
+++ notebooks/python/working_forecast_promo/working_forecast_promo.py
@@ -1,4 +1,5 @@
 # Databricks notebook source
+import uuid
 from typing import List
 
 # COMMAND ----------
@@ -28,21 +29,47 @@ elif technology == 'laser':
 
 # COMMAND ----------
 
-def add_version_and_update_table_sproc(record: str) -> str:
-  f"""
-  CALL prod.addversion_sproc('{record}', 'SYSTEM BUILD');
+# generate a uuid to uniquely identify the record set
+guid = str(uuid.uuid4())
 
-  UPDATE prod.{record.lower()}
-  SET load_date = (SELECT MAX(load_date) FROM prod.version WHERE record = '{record}'),
-  version = (SELECT MAX(version) FROM prod.version WHERE record = '{record}');
-  """
+# COMMAND ----------
+
+def update_table_version_info(
+    configs: dict, version_info: Union[str, datetime], destination: str, dummy_version: str
+) -> None:
+    query = f"""
+    UPDATE {destination}
+    SET load_date = '{version_info[1]}',
+    version = '{version_info[0]}'
+    WHERE version = '{dummy_version}';
+    """
+    submit_remote_query(configs, query)
+    return None
+
 
 def read_stage_write_prod(inputs: List[List[str]]):
-  for input in inputs:
-    df = read_redshift_to_df(configs) \
-      .option("query", input[1]) \
-      .load()
-    write_df_to_redshift(configs=configs, df=df, destination=f"prod.{input[0].lower()}", mode="append", postactions=add_version_and_update_table_sproc(f"{input[0]}_{technology_label}".upper()))
+    for input in inputs:
+        # read in result of query as DataFrame
+        df = read_redshift_to_df(configs).option("query", input[1]).load()
+        # write DataFrame out to prod.working_forecast table
+        destination = f"prod.{input[0].lower()}"
+
+        write_df_to_redshift(
+            configs=configs, df=df, destination=destination, mode="append"
+        )
+
+        # add new version
+        addversion_info = call_redshift_addversion_sproc(
+            configs, "IE2-WORKING-FORECAST", "SYSTEM BUILD"
+        )
+
+        # update inserted records with new version info
+        update_table_version_info(
+            configs=configs,
+            version_info=addversion_info,
+            destination=destination,
+            dummy_version=guid,
+        )
 
 # COMMAND ----------
 
@@ -60,13 +87,13 @@ SELECT
     , cartridges
     , channel_fill
     , supplies_spares_cartridges
-    ,{'0 AS' if technology == 'laser' else ''} host_cartridges
-    ,{'0 AS' if technology == 'laser' else ''} welcome_kits
+    ,{'0 AS' if technology == 'laser' else ''} host_cartridges --default to 0 for laser
+    ,{'0 AS' if technology == 'laser' else ''} welcome_kits --default to 0 for laser
     , expected_cartridges
     , vtc
     , adjusted_cartridges
-    , CAST(NULL AS DATE) load_date
-    , CAST(NULL AS varchar(64)) version
+    , CAST(NULL AS DATE) load_date --create a dummy load_date which will be replaced after calling prod.addversion_sproc
+    , CAST('{guid}' AS varchar(64)) version --create a dummy version which will be replaced after calling prod.addversion_sproc
 FROM scen.{technology_label}_working_fcst
 """
 
