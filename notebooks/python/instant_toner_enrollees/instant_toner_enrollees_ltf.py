2051674c6a7a6ddbd9bacffaacda5891d0cbf45c
alex.selvasingh@hp.com <alex.selvasingh@hp.com>
2023-05-24 06:48:48 +0000

    Added scripts for loading IToner data

--- notebooks/python/instant_toner_enrollees/instant_toner_enrollees_ltf.py
+++ notebooks/python/instant_toner_enrollees/instant_toner_enrollees_ltf.py
@@ -7,27 +7,31 @@
 
 # COMMAND ----------
 
-df_iink_ltf = spark.read.format('csv').options(header='true', inferSchema='true').load('{}product/ib/instant_ink_enrollees/instant_ink_enrollees_ltf/i_ink_ltf.csv'.format(constants['S3_BASE_BUCKET'][stack]))
+df_instant_toner_ltf = spark.read.format('csv').options(header='true', inferSchema='true').load('{}product/ib/instant_toner_enrollees/instant_toner_enrollees_ltf/i_toner_enrollees_ltf.csv'.format(constants['S3_BASE_BUCKET'][stack]))
 
 # COMMAND ----------
 
-write_df_to_redshift(configs, df_iink_ltf, "stage.instant_ink_enrollees_ltf", "overwrite")
+df_instant_toner_ltf.printSchema()
 
 # COMMAND ----------
 
- query_set_previous_official_zero = """
- UPDATE prod.instant_ink_enrollees_ltf
- SET official  = 0 
- """
+write_df_to_redshift(configs, df_instant_toner_ltf, "stage.instant_toner_enrollees_ltf", "overwrite")
 
 # COMMAND ----------
 
-submit_remote_query(configs, query_set_previous_official_zero)
+#  query_set_previous_official_zero = """
+#  UPDATE prod.instant_toner_enrollees_ltf
+#  SET official  = 0 
+#  """
+
+# COMMAND ----------
+
+# submit_remote_query(configs, query_set_previous_official_zero)
 
 # COMMAND ----------
 
 query_add_version = """
-CALL prod.addversion_sproc('IINK_ENROLLEES_LTF', 'FORECASTER INPUT');  
+CALL prod.addversion_sproc('ITONER_ENROLLEES_LTF', 'FORECASTER INPUT');  
 """
 
 # COMMAND ----------
@@ -36,7 +40,7 @@ submit_remote_query(configs, query_add_version)
 
 # COMMAND ----------
 
-final_iink_enrollees_ltf = """
+final_itoner_enrollees_ltf = """
 SELECT
  cast(calendar_month_date as date) cal_date
 ,fiscal_qtr
@@ -44,8 +48,8 @@ SELECT
 ,country
 ,market10
 ,region_3
-,program_type
-,program_type2
+--,program_type
+--,program_type2
 ,sub_brand
 ,series_name
 ,cast(series_number as int) series_number
@@ -57,23 +61,23 @@ SELECT
 ,hw_sellto
 ,p1_enrollees
 ,p2_enrollees
-,partial_enrollees
-,enroll_customer
-,enroll_replacement
+--,partial_enrollees
+--,enroll_customer
+--,enroll_replacement
 ,gross_new_enrollees
-,p1_cancels
-,p2_cancels
-,partial_cancels
+--,p1_cancels
+--,p2_cancels
+--,partial_cancels
 ,cancel_customer
 ,cancel_replacement
 ,gross_cancels
 ,net_enrollees
-,net_p1_enrollees
-,net_p2_enrollees
+--,net_p1_enrollees
+--,net_p2_enrollees
 ,cumulative_enrollees
-,cumulative_p1_enrollees
-,cumulative_p2_enrollees
-,prior_cum
+--,cumulative_p1_enrollees
+--,cumulative_p2_enrollees
+--,prior_cum
 ,qtr_cumulative_enrollees
 ,fcst_type
 ,data_type
@@ -82,21 +86,21 @@ SELECT
 ,NULL load_date
 ,NULL version
 ,1 official
-FROM stage.instant_ink_enrollees_ltf
+FROM stage.instant_toner_enrollees_ltf
 """
 
-df_final_iink_enrollees_ltf = read_redshift_to_df(configs).option("query", final_iink_enrollees_ltf).load()
+df_final_itoner_enrollees_ltf = read_redshift_to_df(configs).option("query", final_itoner_enrollees_ltf).load()
 
 # COMMAND ----------
 
-write_df_to_redshift(configs, df_final_iink_enrollees_ltf, "prod.instant_ink_enrollees_ltf", "append")
+write_df_to_redshift(configs, df_final_itoner_enrollees_ltf, "prod.instant_toner_enrollees_ltf", "append")
 
 # COMMAND ----------
 
  query_update_latest_version =  """
- UPDATE prod.instant_ink_enrollees_ltf
- SET version  = (SELECT MAX(version) FROM prod.version WHERE record = 'IINK_ENROLLEES_LTF'),
-     load_date = (SELECT MAX(load_date) FROM prod.version WHERE record = 'IINK_ENROLLEES_LTF')
+ UPDATE prod.instant_toner_enrollees_ltf
+ SET version  = (SELECT MAX(version) FROM prod.version WHERE record = 'ITONER_ENROLLEES_LTF'),
+     load_date = (SELECT MAX(load_date) FROM prod.version WHERE record = 'ITONER_ENROLLEES_LTF')
  WHERE version IS NULL
  """
 
--- notebooks/python/instant_toner_enrollees/instant_toner_enrollees_stf.py
+++ notebooks/python/instant_toner_enrollees/instant_toner_enrollees_stf.py
@@ -7,27 +7,27 @@
 
 # COMMAND ----------
 
-df_iink_stf = spark.read.format('csv').options(header='true', inferSchema='true').load('{}product/ib/instant_ink_enrollees/instant_ink_enrollees_stf/i_ink_stf.csv'.format(constants['S3_BASE_BUCKET'][stack]))
+df_itoner_stf = spark.read.format('csv').options(header='true', inferSchema='true').load('{}product/ib/instant_toner_enrollees/instant_toner_enrollees_stf/i_toner_enrollees_stf.csv'.format(constants['S3_BASE_BUCKET'][stack]))
 
 # COMMAND ----------
 
-write_df_to_redshift(configs, df_iink_stf, "stage.instant_ink_enrollees_stf", "overwrite")
+write_df_to_redshift(configs, df_itoner_stf, "stage.instant_toner_enrollees_stf", "overwrite")
 
 # COMMAND ----------
 
- query_set_previous_official_zero = """
- UPDATE prod.instant_ink_enrollees_stf 
- SET official  = 0 
- """
+#  query_set_previous_official_zero = """
+#  UPDATE prod.instant_toner_enrollees_stf 
+#  SET official  = 0 
+#  """
 
 # COMMAND ----------
 
-submit_remote_query(configs, query_set_previous_official_zero)
+# submit_remote_query(configs, query_set_previous_official_zero)
 
 # COMMAND ----------
 
 query_add_version = """
-CALL prod.addversion_sproc('IINK_ENROLLEES_STF', 'FORECASTER INPUT');  
+CALL prod.addversion_sproc('ITONER_ENROLLEES_STF', 'FORECASTER INPUT');  
 """
 
 # COMMAND ----------
@@ -36,16 +36,16 @@ submit_remote_query(configs, query_add_version)
 
 # COMMAND ----------
 
-final_iink_enrollees_stf = """
+final_itoner_enrollees_stf = """
 SELECT
- cast(calendar_month_date as date) cal_date
+cast(calendar_month_date as date) cal_date
 ,fiscal_qtr
 ,fiscal_yr
 ,country
 ,market10
 ,region_3
-,program_type
-,program_type2
+--,program_type
+--,program_type2
 ,sub_brand
 ,series_name
 ,cast(series_number as int) series_number
@@ -57,22 +57,22 @@ SELECT
 ,hw_sellto
 ,p1_enrollees
 ,p2_enrollees
-,partial_enrollees
+--,partial_enrollees
 ,enroll_customer
 ,enroll_replacement
 ,gross_new_enrollees
-,p1_cancels
-,p2_cancels
-,partial_cancels
+--,p1_cancels
+--,p2_cancels
+--,partial_cancels
 ,cancel_customer
 ,cancel_replacement
 ,gross_cancels
 ,net_enrollees
-,net_p1_enrollees
-,net_p2_enrollees
+--,net_p1_enrollees
+--,net_p2_enrollees
 ,cumulative_enrollees
-,cumulative_p1_enrollees
-,cumulative_p2_enrollees
+--,cumulative_p1_enrollees
+--,cumulative_p2_enrollees
 ,prior_cum
 ,qtr_cumulative_enrollees
 ,fcst_type
@@ -82,21 +82,21 @@ SELECT
 ,NULL load_date
 ,NULL version
 ,1 official
-FROM stage.instant_ink_enrollees_stf
+FROM stage.instant_toner_enrollees_stf
 """
 
-df_final_iink_enrollees_stf = read_redshift_to_df(configs).option("query", final_iink_enrollees_stf).load()
+df_final_itoner_enrollees_stf = read_redshift_to_df(configs).option("query", final_itoner_enrollees_stf).load()
 
 # COMMAND ----------
 
-write_df_to_redshift(configs, df_final_iink_enrollees_stf, "prod.instant_ink_enrollees_stf", "append")
+write_df_to_redshift(configs, df_final_itoner_enrollees_stf, "prod.instant_toner_enrollees_stf", "append")
 
 # COMMAND ----------
 
  query_update_latest_version = """
- UPDATE prod.instant_ink_enrollees_stf
- SET version  = (SELECT MAX(version) FROM prod.version WHERE record = 'IINK_ENROLLEES_STF'),
-     load_date = (SELECT MAX(load_date) FROM prod.version WHERE record = 'IINK_ENROLLEES_STF')
+ UPDATE prod.instant_toner_enrollees_stf
+ SET version  = (SELECT MAX(version) FROM prod.version WHERE record = 'ITONER_ENROLLEES_STF'),
+     load_date = (SELECT MAX(load_date) FROM prod.version WHERE record = 'ITONER_ENROLLEES_STF')
  WHERE version IS NULL
  """
 
