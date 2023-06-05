92ff3a6de57ce8ad2ac718d1ad852510a2918010
Matt Koson <matt.koson@hp.com>
2023-06-05 16:26:22 -0700

    Update delta_lake_load_with_params.py

--- notebooks/python/common/delta_lake_load_with_params.py
+++ notebooks/python/common/delta_lake_load_with_params.py
@@ -7,6 +7,10 @@
 
 # COMMAND ----------
 
+from pyspark.sql import functions as F
+
+# COMMAND ----------
+
 # MAGIC %run ../common/configs
 
 # COMMAND ----------
@@ -20,23 +24,37 @@ import re
 for table in tables:
     # Define the input and output formats and paths and the table name.
     full_table_name = table[0]
+    schema = table[0].split(".")[0]
+    table_name = table[0].split(".")[1]
     mode = table[2]
+    write_format = 'delta'
+    save_path = f'/tmp/delta/{schema}/{table_name}'
 
+    # Delete old table
+    #print(f'dropping {table[0]}...')
+    #spark.sql("DROP TABLE IF EXISTS " + table[0])
+    #print(f'{table[0]} dropped')
+    
     # Load the data from its source.
     df = table[1]
     print(f'loading {table[0]}...')
     
     for column in df.dtypes:
-        column_wo_parentheses = re.sub('\(|\)', '', column[0].lower())
-        column_w_underscores = re.sub('-| |/', '_', column_wo_parentheses)
-        renamed_column = re.sub('\$', '_dollars', column_w_underscores)
+        renamed_column = re.sub('\)', '', re.sub('\(', '', re.sub('-', '_', re.sub('/', '_', re.sub('\$', '_dollars', re.sub(' ', '_', column[0])))))).lower()
         df = df.withColumnRenamed(column[0], renamed_column)   
     
     # Write the data to its target.
     df.write \
-      .format('delta') \
+      .format(write_format) \
       .mode(mode) \
       .option("overwriteSchema", "true")\
-      .saveAsTable(table[0])
+      .save(save_path)
 
+    spark.sql(f"CREATE SCHEMA IF NOT EXISTS {schema}")
+    
+    # Create the table.
+    spark.sql("CREATE TABLE IF NOT EXISTS " + table[0] + " USING DELTA LOCATION '" + save_path + "'")
+    
+    spark.table(table[0]).createOrReplaceTempView(table_name)
+    
     print(f'{table[0]} loaded')
