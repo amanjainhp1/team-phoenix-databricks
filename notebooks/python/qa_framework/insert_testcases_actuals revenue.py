86e90a8ac2de8b8dd6b152e2d7515099c51d4388
shreyashree.misra@hp.com <shreyashree.misra@hp.com>
2023-06-27 15:03:20 +0000

    inserted queries jun 27

--- notebooks/python/qa_framework/insert_testcases_actuals revenue.py
+++ notebooks/python/qa_framework/insert_testcases_actuals revenue.py
@@ -60,6 +60,38 @@ from fin_prod.actuals_supplies_baseprod asb group by cal_date ,country_alpha2, m
 from fin_prod.actuals_supplies_salesprod group by cal_date ,country_alpha2, currency, market10  , sales_product_number , pl,customer_engagement ,version having count_dup > 1','','1','1',getdate(),'admin',1,'Critical')
 ,('Phoenix - QA','','','MDM check','Actuals Revenue','Check for missing base product number values in MDM','fin_prod',' fin_prod.actuals_supplies_baseprod','units',
 'select distinct base_product_number,platform_subset ,country_alpha2 ,pl  from fin_prod.actuals_supplies_baseprod where base_product_number in (select distinct base_product_number from fin_prod.actuals_supplies_baseprod except select distinct base_product_number from mdm.rdma_base_to_sales_product_map)','','1','1',getdate(),'admin',1,'Critical')
+"""
+
+# COMMAND ----------
+
+submit_remote_query(configs,insert_testqueries_in_testcases_table_query ) # insert into test cases table
+
+# COMMAND ----------
+
+insert_testqueries_in_testcases_table_query= f""" 
+INSERT INTO stage.test_cases
+(project, server_name, database_name, test_category, module_name, test_case_name,schema_name, table_name, element_name, test_query, query_path, min_threshold, max_threshold, test_case_creation_date, test_case_created_by,enabled,severity)
+VALUES 
+('Phoenix - QA','','','MDM check','Actuals Revenue','Check for missing sales product number values in MDM','fin_prod',' fin_prod.actuals_supplies_salesprod','units',
+'select distinct sales_product_number,country_alpha2 ,pl  from fin_prod.actuals_supplies_salesprod where sales_product_number in (select distinct sales_product_number from fin_prod.actuals_supplies_salesprod except select distinct sales_product_number from mdm.rdma_base_to_sales_product_map)','','1','1',getdate(),'admin',1,'Critical')
+,('Phoenix - QA','','','Biz Validation testing','Actuals Revenue','Revenue without US','fin_prod','fin_prod.actuals_supplies_baseprod','units','select platform_subset,country_alpha2  from fin_prod.actuals_supplies_baseprod
+group by platform_subset,country_alpha2 having sum(net_revenue)<>0 except select platform_subset,geography from prod.usage_share_country where version in (select max(version) from  prod.usage_share_country )
+group by platform_subset,geography having max(units)<>0','','1','1',getdate(),'admin',1,'Medium')
+,('Phoenix - QA','','','Biz Validation testing','Actuals Revenue','Drop in PS_Country between Usage Share and Actuals revenue','fin_prod','fin_prod.actuals_supplies_baseprod','units','select platform_subset,geography from prod.usage_share_country where version in (select max(version) from  prod.usage_share_country )
+and cal_date<='2023-05-01' group by platform_subset,geography having max(units)<>0 except select platform_subset,country_alpha2  from fin_prod.actuals_supplies_baseprod
+group by platform_subset,country_alpha2 having sum(net_revenue)<>0','','1','1',getdate(),'admin',1,'Medium')
+,('Phoenix - QA','','','Biz Validation testing','Actuals Revenue','Drop in PS_Country between IB and Actuals revenue','fin_prod','fin_prod.actuals_supplies_baseprod','units','select platform_subset,country_alpha2 from prod.ib where version in (select max(version) from  prod.ib where official=1 )
+and cal_date<='2023-05-01' group by platform_subset,country_alpha2 having max(units)<>0 except select platform_subset,country_alpha2  from fin_prod.actuals_supplies_baseprod
+group by platform_subset,country_alpha2 having sum(net_revenue)<>0','','1','1',getdate(),'admin',1,'Medium')
+,('Phoenix - QA','','','Duplicate check','Actuals Revenue','Check for duplicate records','prod','prod.actuals_supplies','units',' select cal_date , country_alpha2, market10, platform_subset, customer_engagement ,version, count(1) count_dup  from prod.actuals_supplies as2  where version = (select max(version) from prod.actuals_supplies as2 where official = 1)
+ group by cal_date , country_alpha2, market10, platform_subset, customer_engagement,version  having count_dup >1','','1','1',getdate(),'admin',1,'Critical')
+,('Phoenix - QA','','','Data availability check','Actuals Revenue','Check if base quantity is negative','prod','prod.actuals_supplies','base_quantity',
+'select * from prod.actuals_supplies where base_quantity < 0 and version = (select max(version) from prod.actuals_supplies as2 where official = 1)','','1','1',getdate(),'admin',1,'Critical')
+,('Phoenix - QA','','','Data availability check','Actuals Revenue','Check if base product number is available','prod','prod.actuals_supplies','base_product_number',
+'select * from prod.actuals_supplies where base_product_number is null or base_product_number=''''','','1','1',getdate(),'admin',1,'Critical')
+,('Phoenix - QA','','','Data availability check','Actuals Revenue','Check if base quantity is available','prod','prod.actuals_supplies','base_quantity',
+'select * from prod.actuals_supplies where base_quantity = 0','','1','1',getdate(),'admin',1,'Critical')
+"""
 
 # COMMAND ----------
 
